0ede215d70d1db1132189a87eabd29a96e20e777
이형석 <gudtjr2949@naver.com>
2023-07-04 17:30:06 +0900

    [백준 1647] 도시 분할 계획 풀이중

---
+++ 백준/BOJ_1647.java
@@ -0,0 +1,11 @@
+package coding_test.백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 백준 1647 : 도시 분할 계획
+public class BOJ_1647 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
