c7b997f1f4e3062cc30eaa4e73bb96b4d973479f
hyeongseok Lee <gudtjr2949@naver.com>
2024-02-28 13:08:06 +0900

    [Gold IV] Title: 작업, Time: 888 ms, Memory: 82432 KB -BaekjoonHub

---
+++ 백준/Gold/2056. 작업/작업.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int[] edgeCnt, time;
+    static List<List<Integer>> adj = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+
+        edgeCnt = new int[N + 1];
+        time = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            time[i] = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) {
+                int first = Integer.parseInt(st.nextToken());
+                adj.get(first).add(i);
+                edgeCnt[i]++;
+            }
+        }
+
+        int answer = solve();
+
+        System.out.println(answer);
+    }
+
+    static int solve() {
+        Queue<Integer> Q = new LinkedList<>();
+        int[] dp = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            dp[i] = time[i];
+            if (edgeCnt[i] == 0) Q.add(i);
+        }
+
+        while (!Q.isEmpty()) {
+//            int size = Q.size();
+//            for (int i = 0; i < size; i++) {
+            int num = Q.poll();
+            for (int next : adj.get(num)) {
+                edgeCnt[next]--;
+                dp[next] = Math.max(dp[next], dp[num] + time[next]);
+                if (edgeCnt[next] == 0) Q.add(next);
+            }
+        }
+//        }
+
+        // 만약 덜 끝낸 작업이 있으면?
+
+        int total = 0;
+
+        for (int i = 1; i <= N; i++) {
+            total = Math.max(total, dp[i]);
+        }
+
+        return total;
+    }
+}
