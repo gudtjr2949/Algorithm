3c2190718af270c085feabd3e9a5911eb4285af4
hyeongseok Lee <gudtjr2949@naver.com>
2024-05-07 15:38:15 +0900

    [Gold IV] Title: 램프, Time: 156 ms, Memory: 15508 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.java
+++ 백준/Gold/1034. 램프/램프.java
@@ -1,72 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, M, K, answer;
-	static int[][] map;
+    static int N, M, K, answer;
+    static char[][] map;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new char[N][M];
 
-		map = new int[N][M];
+        for (int i = 0 ; i < N ; i++) {
+            String s = bf.readLine();
+            for (int j = 0 ; j < M ; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
 
-		answer = 0;
+        K = Integer.parseInt(bf.readLine());
 
-		for (int i = 0 ; i < N ; i++) {
-			String s = bf.readLine();
-			for (int j = 0 ; j < M ; j++) {
-				map[i][j] = s.charAt(j) - '0';
-			}
-		}
+        solve();
 
-		K = Integer.parseInt(bf.readLine());
+        System.out.println(answer);
+    }
 
-		for (int i = 0 ; i < N ; i++) {
-			int cnt = 0;
+    static void solve() {
+        for (int i = 0 ; i < N ; i++) {
+            int cnt = 0;
+            StringBuilder standard = new StringBuilder();
 
-			for (int j = 0 ; j < M ; j++) {
-				if (map[i][j] == 0) cnt++;
-			}
+            for (int j = 0 ; j < M ; j++) {
+                standard.append(map[i][j]);
+                if (map[i][j] == '0') cnt++;
+            }
 
-			if (K >= cnt) {
-				// K와 한 행의 0의 갯수가 모두 홀수이거나 K와 한 행의 0의 갯수가 모두 짝수인 경우
-				if ((K % 2 != 0 && cnt % 2 != 0) || (K % 2 == 0 && cnt % 2 == 0)) {
-					 // 같은 모양의 행의 갯수 찾기
-					findSameLamp(i);
-				} 
-			}
-		}
+            int sum = 0;
 
-		System.out.println(answer);
-	}
+            if (cnt <= K && (K % 2 != 0 && cnt % 2 != 0 || K % 2 == 0 && cnt % 2 == 0)) {
+                for (int j = 0 ; j < N ; j++) {
+                    StringBuilder tmp = new StringBuilder();
 
-	private static void findSameLamp(int idx) {
-		int cnt = 1;
+                    for (int k = 0 ; k < M ; k++) {
+                        tmp.append(map[j][k]);
+                    }
 
-		String s1 = "";
+                    if (standard.toString().equals(tmp.toString())) sum++;
+                }
+            }
 
-		for (int i = 0 ; i < M ; i++) {
-			s1 += map[idx][i];
-		}
-
-		for (int i = 0 ; i < N ; i++) {
-			if (idx != i) {
-				String s2 = "";
-				for (int j = 0; j < M; j++) {
-					s2 += map[i][j];
-				}
-
-				if (s1.equals(s2))
-					cnt++;
-			}
-		}
-
-		answer = Math.max(answer, cnt);
-	}
+            answer = Math.max(answer, sum);
+        }
+    }
 }
