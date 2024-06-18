1b0a8dc5eb5a8ff6d9ee9d0dbb4298de482f8616
hyeongseok Lee <gudtjr2949@naver.com>
2024-06-18 17:41:52 +0900

    [Silver I] Title: 망가진 키보드, Time: 368 ms, Memory: 29260 KB -BaekjoonHub

---
+++ 백준/Silver/6503. 망가진 키보드/망가진 키보드.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int M, answer;
+    static String s;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while ((M = Integer.parseInt(bf.readLine())) != 0) {
+            s = bf.readLine();
+            answer = 0;
+            solve();
+            sb.append(answer).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void solve() {
+        int left = -1;
+        int right = -1;
+
+        int[] check = new int[128];
+
+        int cnt = 0; // 사용한 자판 수
+
+//        check[s.charAt(0)] = 1;
+
+        while (left <= right) {
+            if (right+1 >= s.length()) break;
+
+            // 사용한 자판의 수가 M보다 작거나, M개의 자판이 필요한 상태지만, 다음 자판은 이미 사용한 적이 있는 자판이라면
+            if (cnt < M || (cnt == M && check[s.charAt(right+1)] >= 1)) {
+                right++;
+                check[s.charAt(right)]++;
+                if (check[s.charAt(right)] == 1) cnt++;
+            } else {
+                left++;
+                check[s.charAt(left)]--;
+                if (check[s.charAt(left)] == 0) cnt--;
+            }
+
+            answer = Math.max(answer, right - left);
+        }
+    }
+}
