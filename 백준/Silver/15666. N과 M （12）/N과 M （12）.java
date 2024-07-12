8ddba2bce9cfa9e6ec303b77cfe4d23e48f9af88
hyeongseok Lee <gudtjr2949@naver.com>
2024-07-12 12:20:01 +0900

    [Silver II] Title: N과 M (12), Time: 112 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr, input;
+    static boolean[] visited;
+    static StringBuilder answer;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        visited = new boolean[N];
+        input = new int[M];
+
+        answer = new StringBuilder();
+
+        st = new StringTokenizer(bf.readLine());
+
+        for (int i = 0 ; i < N ; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+
+        System.out.println(answer);
+    }
+
+    static void dfs(int idx, int cur) {
+        if (idx == M) {
+            for (int i = 0 ; i < M ; i++) {
+                answer.append(input[i]).append(" ");
+            }
+            answer.append("\n");
+
+            return;
+        }
+
+        int before = 0;
+
+        for (int i = cur ; i < N ; i++) {
+            if (arr[i] != before) {
+                before = arr[i];
+                input[idx] = arr[i];
+                dfs(idx + 1, i);
+            }
+        }
+    }
+}
