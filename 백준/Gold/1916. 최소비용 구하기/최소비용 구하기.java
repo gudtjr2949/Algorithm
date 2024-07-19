6ede16d9d7b487a5c4463363932e75b91495ac75
hyeongseok Lee <gudtjr2949@naver.com>
2024-07-19 11:40:25 +0900

    [Gold V] Title: 최소비용 구하기, Time: 460 ms, Memory: 50440 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -4,39 +4,33 @@ import java.util.*;
 
 public class Main {
 
-    static int N;
+    static int N, M;
     static List<List<Node>> adj;
+    static class Node {
+        int idx, cost;
 
-    static class Node implements Comparable<Node> {
-        int v, cost;
-
-        public Node(int v, int cost) {
-            this.v = v;
+        public Node(int idx, int cost) {
+            this.idx = idx;
             this.cost = cost;
         }
-
-        @Override
-        public int compareTo(Node n) {
-            return this.cost - n.cost;
-        }
     }
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
+        M = Integer.parseInt(bf.readLine());
 
         adj = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            adj.add(new ArrayList<>());
-        }
+        for (int i = 0 ; i <= N ; i++) adj.add(new ArrayList<>());
 
-        int M = Integer.parseInt(bf.readLine());
-        for (int i = 0; i < M; i++) {
+        for (int i = 0 ; i < M ; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            adj.get(u).add(new Node(v, cost));
+
+            adj.get(from).add(new Node(to, cost));
         }
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
@@ -44,31 +38,38 @@ public class Main {
         System.out.println(dijk(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
     }
 
-    static int dijk(int start, int end) {
-        PriorityQueue<Node> PQ = new PriorityQueue<>();
-        boolean[] visited = new boolean[N + 1];
-        int[] dp = new int[N + 1];
+    private static int dijk(int start, int end) {
+        PriorityQueue<Node> PQ = new PriorityQueue<>(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.cost - o2.cost;
+            }
+        });
 
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        boolean[] visited = new boolean[N+1];
+        int[] dist = new int[N+1];
+
+        Arrays.fill(dist, 100_000_001);
+        dist[start] = 0;
 
         PQ.add(new Node(start, 0));
-        dp[start] = 0;
 
         while (!PQ.isEmpty()) {
             Node now = PQ.poll();
 
-            if (!visited[now.v]) {
-                visited[now.v] = true;
-                for (Node next : adj.get(now.v)) {
-                    if (!visited[next.v] && dp[next.v] > now.cost + next.cost) {
-                        visited[now.v] = true;
-                        dp[next.v] = now.cost + next.cost;
-                        PQ.add(new Node(next.v, dp[next.v]));
-                    }
+            if (visited[now.idx]) continue;
+            visited[now.idx] = true;
+
+            if (now.idx == end) break;
+
+            for (Node next : adj.get(now.idx)) {
+                if (dist[next.idx] > dist[now.idx] + next.cost) {
+                    dist[next.idx] = dist[now.idx] + next.cost;
+                    PQ.add(new Node(next.idx, dist[next.idx]));
                 }
             }
         }
-
-        return dp[end];
+        
+        return dist[end];
     }
 }
