6f887928cfaf8cab0f5478124a6501d3e6a0ecfb
hyeongseok Lee <gudtjr2949@naver.com>
2024-08-20 13:37:05 +0900

    [Silver I] Title: 기타 레슨, Time: 280 ms, Memory: 23996 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -4,48 +4,44 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M, sum = 0;
+    static int N, M, answer, sum;
     static int[] arr;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         arr = new int[N];
 
         st = new StringTokenizer(bf.readLine());
-
         for (int i = 0 ; i < N ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             sum += arr[i];
         }
 
-        binarySearch();
-    }
-
-    static void binarySearch() {
-        int left = 1;
-        int right = sum;
-        int answer = 0;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            if (solve(mid) > M) {
-                left = mid + 1;
-            } else {
-                answer = mid;
-                right = mid - 1;
-            }
-        }
+        solve();
 
         System.out.println(answer);
     }
 
-    static int solve(int mid) {
+    static void solve() {
+        int left = 0;
+        int right = 100_000 * 10_000 + 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (countBluray(mid) > M) {
+                left = mid+1;
+            } else {
+                answer = mid;
+                right = mid-1;
+            }
+        }
+    }
+
+    static int countBluray(int mid) {
         int cnt = 1;
         int length = 0;
 
@@ -54,11 +50,11 @@ public class Main {
                 return M+1;
             }
 
-            if (length + arr[i] <= mid) {
-                length += arr[i];
-            } else {
+            if (length + arr[i] > mid) {
                 length = arr[i];
                 cnt++;
+            } else {
+                length += arr[i];
             }
         }
 
