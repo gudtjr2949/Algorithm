1d7b4456bd4a5c5546c0db8f775fcbf46a583f18
hyeongseok Lee <gudtjr2949@naver.com>
2024-08-28 12:12:12 +0900

    [Silver II] Title: 병사 배치하기, Time: 132 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -4,35 +4,38 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N, max;
+    static int[] arr, dp;
+
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int[] arr = new int[N];
-        int[] dp = new int[N];
+        N = Integer.parseInt(bf.readLine());
+        arr = new int[N];
+        dp = new int[N];
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        for (int i = 0 ; i < N ; i++) {
+        for (int i = 0 ; i < N ; i++)
             arr[i] = Integer.parseInt(st.nextToken());
-        }
 
         Arrays.fill(dp, 1);
 
+        solve();
 
-        for (int i = 1 ; i < N ; i++) {
-            for (int j = 0 ; j < i ; j++) {
+        System.out.println(N - max);
+    }
+
+    static void solve() {
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = 0 ; j <= i ; j++) {
                 if (arr[i] < arr[j]) {
                     dp[i] = Math.max(dp[i], dp[j]+1);
                 }
             }
         }
 
-        int max = 0;
-
         for (int i = 0 ; i < N ; i++) {
             max = Math.max(max, dp[i]);
         }
-
-        System.out.println(N - max);
     }
 }
