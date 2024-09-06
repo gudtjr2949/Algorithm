129b8887cabcaf7df5ab4c49ab4f28148d2c46c0
hyeongseok Lee <gudtjr2949@naver.com>
2024-09-06 12:37:08 +0900

    [Gold IV] Title: 문자열 폭발, Time: 508 ms, Memory: 61804 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -3,42 +3,46 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
+
+    static String s, boom;
+    static StringBuilder sb;
+
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String s = bf.readLine();
-        String bomb = bf.readLine();
+        s = bf.readLine();
+        boom = bf.readLine();
+        sb = new StringBuilder();
 
+        solve();
+
+        if (sb.length() == 0) System.out.println("FRULA");
+        else System.out.println(sb.reverse());
+    }
+
+    static void solve() {
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0 ; i < s.length() ; i++) {
-            stack.push(s.charAt(i));
+            stack.add(s.charAt(i));
 
-            if (stack.size() >= bomb.length()) {
-                boolean same = true;
-
-                for (int j = 0 ; j < bomb.length() ; j++) {
-                    if (stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
-                        same = false;
+            if (stack.size() >= boom.length()) {
+                boolean flag = true;
+                for (int j = 0 ; j < boom.length() ; j++) {
+                    if (stack.get(stack.size() - boom.length() + j) != boom.charAt(j)) {
+                        flag = false;
                         break;
                     }
                 }
 
-                if (same) {
-                    for (int j = 0 ; j < bomb.length() ; j++) stack.pop();
+                if (flag) {
+                    for (int j = 0 ; j < boom.length() ; j++) stack.pop();
                 }
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        if (stack.size() == 0) {
-            System.out.println("FRULA");
-        } else {
-            int size = stack.size();
-            for (int i = 0 ; i < size ; i++) {
-                sb.append(stack.pop());
-            }
 
-            System.out.println(sb.reverse().toString());
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
         }
     }
 }
