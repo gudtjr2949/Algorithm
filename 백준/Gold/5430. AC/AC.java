652b803b0c547f9a65b057507564620c84fc40e7
hyeongseok Lee <gudtjr2949@naver.com>
2024-09-06 12:04:16 +0900

    [Gold V] Title: AC, Time: 692 ms, Memory: 68196 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,75 +1,71 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N;
+    static String p;
+    static StringBuilder sb;
+    static Deque<String> dq = new ArrayDeque<>();
+
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(bf.readLine());
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
 
-        for (int Test = 0 ; Test < T ; Test++) {
-            String order = bf.readLine();
-            int N = Integer.parseInt(bf.readLine());
-            Deque<Integer> deque = new LinkedList<>();
-
+        while (T-- > 0) {
+            p = bf.readLine();
+            N = Integer.parseInt(bf.readLine());
             String s1 = bf.readLine();
             String s2 = s1.replace("[", "");
             String s3 = s2.replace("]", "");
-
             StringTokenizer st = new StringTokenizer(s3, ",");
 
-            for (int i = 0 ; i < N ; i++)
-                deque.add(Integer.parseInt(st.nextToken()));
-
-            int dir = 0; // dir = 0 이면 정방향, dir = 1 이면 역방향
-
-            String s = "[";
-
-            for (int i = 0 ; i < order.length() ; i++) {
-                if (order.charAt(i) == 'R') {
-                    if (dir == 0) dir = 1;
-                    else dir = 0;
-                } else {
-                    if (deque.size() == 0) {
-                        s = "error";
-                        break;
-                    } else if (dir == 0) {
-                        deque.pollFirst();
-                    } else {
-                        deque.pollLast();
-                    }
-                }
-            }
-
-            if (!s.equals("error")) {
-                s = makePrint(dir, deque);
-            }
-
-            sb.append(s).append("\n");
+            sb.append(solve(st)).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    static String makePrint(int dir, Deque<Integer> deque) {
-        StringBuilder s = new StringBuilder("[");
+    static String solve(StringTokenizer st) {
+        for (int i = 0; i < N ; i++) {
+            dq.add(st.nextToken());
+        }
 
-        if (dir == 0) {
-            while (!deque.isEmpty()) {
-                s.append(deque.pollFirst()).append(",");
-            }
-        } else {
-            while (!deque.isEmpty()) {
-                s.append(deque.pollLast()).append(",");
+        boolean error = false;
+        int first = 1;
+
+        for (int i = 0 ; i < p.length() ; i++) {
+            if (p.charAt(i) == 'R') {
+                first *= -1;
+            } else {
+                if (dq.size() == 0) {
+                    return "error";
+                }
+
+                if (first == 1)
+                    dq.pollFirst();
+                else
+                    dq.pollLast();
             }
         }
 
-        if (s.length() > 1) {
-            s.deleteCharAt(s.length() - 1);
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+        while (!dq.isEmpty()) {
+            if (first == 1) {
+                s.append(dq.pollFirst()).append(",");
+            } else {
+                s.append(dq.pollLast()).append(",");
+            }
         }
+
+        if (s.length() > 1)
+            s.deleteCharAt(s.length()-1);
 
         s.append("]");
 
