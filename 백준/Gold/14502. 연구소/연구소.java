bb702085306587e33b9ade8a2cca8f7479995b33
hyeongseok Lee <gudtjr2949@naver.com>
2024-09-14 12:53:29 +0900

    [Gold IV] Title: 연구소, Time: 348 ms, Memory: 136688 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,128 +1,106 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 import java.util.*;
 
-// 백준 14502 : 연구소
 public class Main {
 
     static int N, M, answer;
+    static int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
     static int[][] map;
-    static ArrayList<Point> list;
-    static int[] nx = {0, 1, 0, -1};
-    static int[] ny = {-1, 0, 1, 0};
+    static List<Node> list;
+    static Node[] input;
 
-    public static void main(String[] args) throws Exception{
+    static class Node {
+        int x, y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-
+        StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        answer = Integer.MIN_VALUE;
-
-        list = new ArrayList<>();
         map = new int[N][M];
+        list = new ArrayList<>();
+        input = new Node[3];
 
         for (int i = 0 ; i < N ; i++) {
-            st = new StringTokenizer(bf.readLine(), " ");
+            st = new StringTokenizer(bf.readLine());
             for (int j = 0 ; j < M ; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 0) {
-                    list.add(new Point(j, i));
-                }
+                if (map[i][j] == 0) list.add(new Node(j, i));
             }
         }
 
-        dfs(0, 0, new Point[3]);
+        dfs(0, 0);
 
         System.out.println(answer);
     }
 
-    private static void dfs(int idx, int cur, Point[] input) {
+    static void dfs(int idx, int cur) {
         if (idx == 3) {
-            setWall(input);
+            bfs();
             return;
         }
 
         for (int i = cur ; i < list.size() ; i++) {
             input[idx] = list.get(i);
-            dfs(idx + 1, i + 1, input);
+            dfs(idx+1, i+1);
         }
     }
 
-    private static void setWall(Point[] input) {
-        int[][] copy = copyMap();
-        
-        // 벽 세우기
-        for (int i = 0 ; i < 3 ; i++) {
-            Point p = input[i];
-            copy[p.y][p.x] = 1;
-        }
-        
-        // 바이러스 퍼지게 하기
-        spread(copy);
-    }
+    static void bfs() {
+        Queue<Node> Q = new LinkedList<>();
 
-    private static void spread(int[][] copy) {
-        Queue<Point> Q = new LinkedList<>();
-
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = 0 ; j < M ; j++) {
-                if (copy[i][j] == 2) {
-                    Q.add(new Point(j, i));
-                }
-            }
-        }
-
-        while (!Q.isEmpty()) {
-            Point p = Q.poll();
-
-            for (int i = 0 ; i < 4 ; i++) {
-                int dx = p.x + nx[i];
-                int dy = p.y + ny[i];
-
-                if (!(dx >= M || dx < 0 || dy >= N || dy < 0) && copy[dy][dx] == 0) {
-                    copy[dy][dx] = 2;
-                    Q.add(new Point(dx, dy));
-                }
-            }
-        }
-
-        count(copy);
-    }
-
-    private static void count(int[][] copy) {
-        int cnt = 0;
-
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = 0 ; j < M ; j++) {
-                if (copy[i][j] == 0) {
-                    cnt++;
-                }
-            }
-        }
-
-        answer = Math.max(answer, cnt);
-    }
-
-    private static int[][] copyMap() {
         int[][] tmp = new int[N][M];
-
         for (int i = 0 ; i < N ; i++) {
             for (int j = 0 ; j < M ; j++) {
                 tmp[i][j] = map[i][j];
             }
         }
 
-        return tmp;
-    }
-
-    static class Point {
-        int x, y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+        for (int i = 0 ; i < 3 ; i++) {
+            int y = input[i].y;
+            int x = input[i].x;
+            tmp[y][x] = 1;
         }
+
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = 0 ; j < M ; j++) {
+                if (tmp[i][j] == 2) {
+                    Q.add(new Node(j, i));
+                }
+            }
+        }
+
+
+        while (!Q.isEmpty()) {
+            Node now = Q.poll();
+
+            for (int i = 0 ; i < 4 ; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+
+                if (nx >= 0 && nx < M && ny >= 0 && ny < N && tmp[ny][nx] == 0) {
+                    tmp[ny][nx] = 2;
+                    Q.add(new Node(nx, ny));
+                }
+            }
+        }
+
+        int result = 0;
+
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = 0 ; j < M ; j++) {
+                if (tmp[i][j] == 0) result++;
+            }
+        }
+
+        answer = Math.max(answer, result);
     }
 }
