9bba76fdb5e08fc690597450a3d532a83224a9f0
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-04 14:00:38 +0900

    [Silver II] Title: 추월, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.java
+++ 백준/Silver/2002. 추월/추월.java
@@ -1,26 +1,23 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
     static int N, answer;
-    static String[] start, end;
+    static String[] in, out;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
         answer = N;
 
-        start = new String[N];
-        end = new String[N];
+        in = new String[N];
+        out = new String[N];
 
-        for (int i = 0 ; i < N ; i++) {
-            start[i] = bf.readLine();
-        }
+        for (int i = 0 ; i < N ; i++) in[i] = bf.readLine();
 
-        for (int i = 0 ; i < N ; i++) {
-            end[i] = bf.readLine();
-        }
+        for (int i = 0 ; i < N ; i++) out[i] = bf.readLine();
 
         solve();
 
@@ -32,9 +29,9 @@ public class Main {
 
         for (int i = 0 ; i < N ; i++) {
             for (int j = idx ; j < N ; j++) {
-                if (start[i].equals(end[j])) {
-                    idx = j+1;
+                if (in[i].equals(out[j])) {
                     answer--;
+                    idx = j+1;
                     break;
                 }
             }
