17bdfb2049751b143fe130c614a7e8bc60b8d060
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-11 12:41:59 +0900

    [level 3] Title: 단속카메라, Time: 8.01 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 class Solution {
     
-    static class Route {
+    static class Node {
         int start, end;
-        public Route(int start, int end) {
+        public Node(int start, int end) {
             this.start = start;
             this.end = end;
         }
@@ -13,24 +13,22 @@ class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
         
-        Route[] route = new Route[routes.length];
-        
+        Node[] cars = new Node[routes.length];
         for (int i = 0 ; i < routes.length ; i++) {
-            route[i] = new Route(routes[i][0], routes[i][1]);
+            cars[i] = new Node(routes[i][0], routes[i][1]);
         }
         
-        Arrays.sort(route, new Comparator<Route>() {
+        Arrays.sort(cars, new Comparator<Node>() {
             @Override
-            public int compare(Route r1, Route r2) {
-                return r1.end - r2.end;
+            public int compare(Node n1, Node n2) {
+                return n1.end - n2.end;
             }
         });
         
         int camera = -30001;
-        
-        for (Route r : route) {
-            if (r.start > camera) {
-                camera = r.end;
+        for (Node car : cars) {
+            if (camera < car.start) {
+                camera = car.end;
                 answer++;
             }
         }
