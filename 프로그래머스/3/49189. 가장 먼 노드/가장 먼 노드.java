efac3f654cc47f6cddb68cb993ae35e8274638ea
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-15 12:37:33 +0900

    [level 3] Title: 가장 먼 노드, Time: 41.94 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,84 +2,54 @@ import java.util.*;
 
 class Solution {
     
-    static int N, MAX = 50_001;
+    static int answer;
     static int[] dp;
-    static List<List<Node>> adj;
-    static class Node implements Comparable<Node> {
-        int v, cost;
-        
-        public Node(int v, int cost) {
-            this.v = v;
-            this.cost = cost;
-        }
-        
-        @Override
-        public int compareTo(Node n) {
-            return this.cost - n.cost;
-        }
-    }
+    static List<List<Integer>> adj;
     
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-        
-        N = n;
+        answer = 0;
         adj = new ArrayList<>();
-        for (int i = 0 ; i <= n ; i++) {
+        for (int i = 0 ; i <= n ; i++) 
             adj.add(new ArrayList<>());
-        }
-        
-        dp = new int[n+1];
-        Arrays.fill(dp, MAX);
         
         for (int i = 0 ; i < edge.length ; i++) {
             int a = edge[i][0];
             int b = edge[i][1];
-            adj.get(a).add(new Node(b, 1));
-            adj.get(b).add(new Node(a, 1));
+            adj.get(a).add(b);
+            adj.get(b).add(a);
         }
         
-        dijk();
+        dp = new int[n+1];
         
-        answer = findAnswer();
+        dijk(n);
         
         return answer;
     }
     
-    static void dijk() {
-        PriorityQueue<Node> PQ = new PriorityQueue<>();
-        PQ.add(new Node(1, 0));
-        boolean[] visited = new boolean[N+1];
-        dp[1] = 0;
+    static void dijk(int n) {
+        Arrays.fill(dp, 100_000_000);
         
-        while (!PQ.isEmpty()) {
-            Node now = PQ.poll();
+        Queue<Integer> Q = new LinkedList<>();
+        Q.add(1);
+        dp[1] = 0;
+        int max = 0;
+        
+        while (!Q.isEmpty()) {
+            Integer now = Q.poll();
             
-            if (!visited[now.v]) visited[now.v] = true;
-            
-            for (Node next : adj.get(now.v)) {
-                if (!visited[next.v] && dp[next.v] > now.cost + next.cost) {
-                    dp[next.v] = now.cost + next.cost;
-                    PQ.add(new Node(next.v, dp[next.v]));
+            for (Integer next : adj.get(now)) {
+                if (dp[next] > dp[now] + 1) {
+                    dp[next] = dp[now] + 1;
+                    max = Math.max(max, dp[next]);
+                    Q.add(next);
                 }
             }
         }
-    }
-    
-    static int findAnswer() {
-        int max = 0;
-        for (int i = 1 ; i <= N ; i++) {
-            if (dp[i] != MAX) {
-                max = Math.max(max, dp[i]);
+        
+        for (int i = 1 ; i <= n ; i++) {
+            if (dp[i] == max) {
+                answer++;
             }
         }
-        
-        int cnt = 0;
-        
-        for (int i = 1 ; i <= N ; i++) {
-            if (dp[i] == max) cnt++;
-        }
-        
-        return cnt;
     }
-    
 }
