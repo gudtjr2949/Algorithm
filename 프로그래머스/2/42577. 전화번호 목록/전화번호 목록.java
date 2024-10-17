772c82f2a0afb4fb56d17e1e7486ef22864337ee
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-17 22:43:09 +0900

    [level 2] Title: 전화번호 목록, Time: 361.04 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,19 +4,12 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
-        Arrays.sort(phone_book, (o1, o2) -> o1.length() - o2.length());
-        
-        Set<String> set = new HashSet<>();
-        
-        for (String phone : phone_book) {
-            for (int i = 1 ; i <= phone.length() ; i++) {
-                String s = phone.substring(0, i);
-                if (set.contains(s)) {
-                    return false;
-                }
+        Arrays.sort(phone_book);
+                
+        for (int i = 0 ; i < phone_book.length-1 ; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
             }
-            
-            set.add(phone);
         }
         
         return true;
