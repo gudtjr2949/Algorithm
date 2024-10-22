61d7a3f9c2a89c675466843488c35c03e70adb08
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-22 15:25:31 +0900

    [Gold III] Title: 중량제한, Time: 616 ms, Memory: 67636 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -4,34 +4,24 @@ import java.util.*;
 
 public class Main {
 
-    static int N, M, fromIsland, toIsland, maxWeight;
-    static List<List<Island>> list;
-    static boolean[] visited;
-
-    static class Island {
-        int to, weight;
-
-        public Island (int to, int weight) {
-            this.to = to;
+    static int N, M, from, to, answer, MAX;
+    static List<List<Node>> adj;
+    static class Node {
+        int dest, weight;
+        public Node(int dest, int weight) {
+            this.dest = dest;
             this.weight = weight;
         }
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        list = new ArrayList<>();
-
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        int left = 0;
-        int right = 0;
-
-        for (int i = 0 ; i < N+1 ; i++) {
-            list.add(new ArrayList<>());
+        adj = new ArrayList<>();
+        for (int i = 0 ; i <= N ; i++) {
+            adj.add(new ArrayList<>());
         }
 
         for (int i = 0 ; i < M ; i++) {
@@ -39,57 +29,55 @@ public class Main {
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
-
-            list.get(from).add(new Island (to, weight));
-            list.get(to).add(new Island(from, weight));
-
-            maxWeight = Math.max(maxWeight, weight);
+            adj.get(from).add(new Node(to, weight));
+            adj.get(to).add(new Node(from, weight));
+            MAX = Math.max(MAX, weight);
         }
 
-        right = maxWeight;
-
         st = new StringTokenizer(bf.readLine());
-        fromIsland = Integer.parseInt(st.nextToken());
-        toIsland = Integer.parseInt(st.nextToken());
+        from = Integer.parseInt(st.nextToken());
+        to = Integer.parseInt(st.nextToken());
+
+        solve();
+
+        System.out.println(answer);
+    }
+
+    static void solve() {
+        int left = 0;
+        int right = MAX;
 
         while (left <= right) {
             int mid = (left + right) / 2;
 
-            visited = new boolean[N+1];
-
             if (bfs(mid)) {
-                left = mid + 1;
+                left = mid+1;
             } else {
-                right = mid - 1;
+                right = mid-1;
             }
         }
 
-        System.out.println(right);
+        answer = right;
     }
 
     static boolean bfs(int mid) {
         Queue<Integer> Q = new LinkedList<>();
-
-        Q.add(fromIsland);
+        Q.add(from);
+        boolean[] visited = new boolean[N+1];
+        visited[from] = true;
 
         while (!Q.isEmpty()) {
-            int from = Q.poll();
+            int now = Q.poll();
 
-            visited[from] = true;
-
-            if (from == toIsland) {
+            if (now == to) {
                 return true;
             }
 
-            for (int i = 0 ; i < list.get(from).size() ; i++) {
-                int to = list.get(from).get(i).to;
-                int weight = list.get(from).get(i).weight;
-
-                if (!visited[to] && mid <= weight) {
-                    visited[to] = true;
-                    Q.add(to);
+            for (Node next : adj.get(now)) {
+                if (next.weight >= mid && !visited[next.dest]) {
+                    visited[next.dest] = true;
+                    Q.add(next.dest);
                 }
-
             }
         }
 
