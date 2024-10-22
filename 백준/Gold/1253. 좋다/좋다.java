365c266e01ba2900f5b51220bf4f8924063c9fa4
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-22 12:55:56 +0900

    [Gold IV] Title: 좋다, Time: 148 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -5,54 +5,44 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, answer;
-	static int[] A;
+    static int N, answer;
+    static int[] arr;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(bf.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0 ; i < N ; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
 
-		A = new int[N];
 
-		answer = 0;
+        Arrays.sort(arr);
 
-		StringTokenizer st = new StringTokenizer(bf.readLine());
+        solve();
 
-		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-		}
+        System.out.println(answer);
+    }
 
-		Arrays.sort(A);
+    static void solve() {
+        for (int i = 0 ; i < N ; i++) {
+            int left = 0;
+            int right = N-1;
 
-		solve();
+            while (left < right) {
+                long sum = arr[left] + arr[right];
 
-		System.out.println(answer);
-	}
-
-	private static void solve() {
-		for (int i = 0; i < N; i++) {
-			int start = 0;
-			int end = N - 1;
-			int target = A[i];
-
-			while (start < end) {
-				int sum = A[start] + A[end];
-
-				if (sum == target) {
-					if (start != i && end != i) {
-						answer++;
-						break;
-					} else if (end == i) {
-						end--;
-					} else if (start == i) {
-						start++;
-					}
-				} else if (sum < target) {
-					start++;
-				} else {
-					end--;
-				}
-			}
-		}
-	}
+                if (sum > arr[i]) right--;
+                else if (sum < arr[i]) left++;
+                else {
+                    if (left != i && right != i) {
+                        answer++;
+                        break;
+                    }
+                    else if (right == i) right--;
+                    else left++;
+                }
+            }
+        }
+    }
 }
