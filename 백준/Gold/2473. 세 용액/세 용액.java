24d7e8c5ce84a9240e98835d454d2dfeef883b9e
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-23 13:09:19 +0900

    [Gold III] Title: 세 용액, Time: 200 ms, Memory: 17240 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -11,52 +11,44 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
-
         arr = new long[N];
+        answer = new long[3];
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 0 ; i < N ; i++)
             arr[i] = Long.parseLong(st.nextToken());
-        }
+
 
         Arrays.sort(arr);
 
         solve();
 
-        for (int i = 0; i < 3; i++) {
-            System.out.print(answer[i] + " ");
-        }
+        System.out.println(answer[0] + " " + answer[1] + " " + answer[2]);
     }
 
-    private static void solve() {
-        long nearZero = 0;
-        answer = new long[3];
+    static void solve() {
+        long min = Long.MAX_VALUE;
 
-        nearZero = Long.MAX_VALUE;
-
-        Loop:
-        for (int i = 0; i < N - 2; i++) {
-            int left = i + 1;
-            int right = N - 1;
+        for (int i = 0 ; i < N-2 ; i++) {
+            int left = i+1;
+            int right = N-1;
 
             while (left < right) {
                 long sum = arr[i] + arr[left] + arr[right];
-                if (nearZero > Math.abs(sum)) {
-                    nearZero = Math.abs(sum);
+
+                if (min > Math.abs(sum)) {
                     answer[0] = arr[i];
                     answer[1] = arr[left];
                     answer[2] = arr[right];
+                    min = Math.abs(sum);
                 }
+
 
                 if (sum < 0) {
                     left++;
                 } else {
+                    if (min == 0) return;
                     right--;
-                }
-
-                if (nearZero == 0) {
-                    break Loop;
                 }
             }
         }
