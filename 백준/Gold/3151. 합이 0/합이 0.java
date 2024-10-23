5c875ee5ee5e6681ec181dee408fd6194b5d5017
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-23 13:44:57 +0900

    [Gold IV] Title: 합이 0, Time: 408 ms, Memory: 17124 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -1,81 +1,67 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static long answer;
-	static int[] arr;
+    static int N;
+    static long answer;
+    static int[] arr;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        arr = new int[N];
 
-		N = Integer.parseInt(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		StringTokenizer st = new StringTokenizer(bf.readLine());
+        Arrays.sort(arr);
 
-		arr = new int[N];
+        solve();
 
-		for (int i = 0 ; i < N ; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+        System.out.println(answer);
+    }
 
-		Arrays.sort(arr);
+    static void solve() {
+        for (int i = 0; i < N - 2; i++) {
+            int left = i + 1;
+            int right = N - 1;
 
-		solve();
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
 
-		System.out.println(answer);
-	}
+                if (sum > 0) right--;
+                else {
+                    if (sum == 0) {
 
-	private static void solve() {
-		for (int i = 0 ; i < N ; i++) {
-			int startIdx = i + 1;
-			int endIdx = N - 1;
-			int value = arr[i];
+                        if (arr[left] == arr[right]) {
+                            answer += ((right - left) * (right - left + 1)) / 2;
+                            break;
+                        } else {
+                            int leftCnt = 1;
+                            int rightCnt = 1;
 
-			if (value > 0) {
-				break;
-			}
+                            while (arr[left] == arr[left + 1]) {
+                                leftCnt++;
+                                left++;
+                            }
 
-			// System.out.println(startIdx + " " + endIdx);
-			while (startIdx < endIdx) {
-				int sum = value + arr[startIdx] + arr[endIdx];
+                            while (arr[right] == arr[right - 1]) {
+                                rightCnt++;
+                                right--;
+                            }
 
-				if (sum == 0) {
-					// 이 두 수가 같다면, 이미 정렬을 한 뒤이기 떄문에, 이 두 수 사이에 있는 수도 모두 같음
-					if (arr[startIdx] == arr[endIdx]) {
-						answer += ((endIdx - startIdx) * (endIdx - startIdx + 1)) / 2;
-						break;
-					} else {
-						int startSame = 1;
-						int endSame = 1;
+                            answer += leftCnt * rightCnt;
+                        }
+                    }
 
-						// 같은 수 확인
-						while (arr[startIdx] == arr[startIdx + 1]) {
-							startSame++;
-							startIdx++;
-						}
-
-						while (arr[endIdx] == arr[endIdx - 1]) {
-							endSame++;
-							endIdx--;
-						}
-
-						answer += endSame * startSame;
-					}
-				}
-
-				if (sum > 0) {
-					endIdx--;
-				} else {
-					startIdx++;
-				}
-			}
-		}
-	}
+                    left++;
+                }
+            }
+        }
+    }
 }
