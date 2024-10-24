e9a3e0e7269ff6165c73d36648980f5282ef1c96
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-24 14:09:31 +0900

    [Gold IV] Title: 동전 1, Time: 108 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, K;
+    static int[] arr, dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        dp = new int[K+1];
+        for (int i = 0 ; i < N ; i++)
+            arr[i] = Integer.parseInt(bf.readLine());
+
+        dp[0] = 1;
+        solve();
+
+        System.out.println(dp[K]);
+    }
+
+    static void solve() {
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = arr[i] ; j <= K ; j++) {
+                dp[j] = dp[j] + dp[j-arr[i]];
+            }
+        }
+    }
+}
