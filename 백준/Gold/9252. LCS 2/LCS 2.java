77a1972b3f7ee9879760299ff7152546d7ab9de9
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-29 13:43:44 +0900

    [Gold IV] Title: LCS 2, Time: 136 ms, Memory: 18412 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,59 +1,54 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Stack;
 
 public class Main {
 
-    static String str1, str2;
-    static char[] answer;
-    static int idx = 0;
+    static int N, M;
+    static String s1, s2;
+    static StringBuilder sb;
     static int[][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        str1 = bf.readLine();
-        str2 = bf.readLine();
+        s1 = bf.readLine();
+        s2 = bf.readLine();
+        N = s1.length();
+        M = s2.length();
 
-        dp = new int[1001][1001];
+        dp = new int[N+1][M+1];
 
         solve();
-        System.out.println(dp[str2.length()][str1.length()]);
-        answer = new char[dp[str2.length()][str1.length()]];
-        idx = dp[str2.length()][str1.length()]-1;
-        print(str2.length(), str1.length());
+
+        sb = new StringBuilder();
+
+        findLSC(N, M);
+
+        System.out.println(dp[N][M]);
+        System.out.println(sb);
+    }
+
+    static void findLSC(int i, int j) {
+        if (dp[i][j] == 0) return;
+
+        if (s1.charAt(i-1) == s2.charAt(j-1)) {
+            findLSC(i-1, j-1);
+            sb.append(s1.charAt(i-1));
+        } else {
+            if (dp[i-1][j] < dp[i][j-1]) findLSC(i, j-1);
+            else findLSC(i-1, j);
+        }
     }
 
     static void solve() {
-        for (int i = 1 ; i <= str2.length() ; i++) {
-            for (int j = 1 ; j <= str1.length() ; j++) {
-                if (str2.charAt(i-1) == str1.charAt(j-1)) {
+        for (int i = 1 ; i <= N ; i++) {
+            for (int j = 1 ; j <= M ; j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
                 } else {
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
-        }
-    }
-
-    static void print(int i, int j) {
-        Stack<Character> st = new Stack<>();
-        while(i > 0 && j > 0) {
-            if(i == 0 || j ==0) break;
-
-            if(dp[i][j] == dp[i-1][j]) {
-                i--;
-            }else if(dp[i][j] == dp[i][j-1]) {
-                j--;
-            } else {
-                st.push(str2.charAt(i-1));
-                i--;
-                j--;
-            }
-        }
-
-        while(!st.isEmpty()) {
-            System.out.print(st.pop());
         }
     }
 }
