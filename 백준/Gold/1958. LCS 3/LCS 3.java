05cb6b35426b7667e87e00d996f7ae4c003262a5
hyeongseok Lee <gudtjr2949@naver.com>
2024-10-29 13:51:51 +0900

    [Gold IV] Title: LCS 3, Time: 148 ms, Memory: 19780 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.java
+++ 백준/Gold/1958. LCS 3/LCS 3.java
@@ -3,26 +3,28 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static String str1, str2, str3;
+    static int N, M, K;
+    static String s1, s2, s3;
     static int[][][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        str1 = bf.readLine();
-        str2 = bf.readLine();
-        str3 = bf.readLine();
-
-        dp = new int[101][101][101];
-
+        s1 = bf.readLine();
+        s2 = bf.readLine();
+        s3 = bf.readLine();
+        N = s1.length();
+        M = s2.length();
+        K = s3.length();
+        dp = new int[N+1][M+1][K+1];
         solve();
-        System.out.println(dp[str1.length()][str2.length()][str3.length()]);
+        System.out.println(dp[N][M][K]);
     }
 
     static void solve() {
-        for (int i = 1 ; i <= str1.length() ; i++) {
-            for (int j = 1 ; j <= str2.length() ; j++) {
-                for (int k = 1 ; k <= str3.length() ; k++) {
-                    if (str1.charAt(i-1) == str2.charAt(j-1) && str2.charAt(j-1) == str3.charAt(k-1)) {
+        for (int i = 1 ; i <= N ; i++) {
+            for (int j = 1 ; j <= M ; j++) {
+                for (int k = 1 ; k <= K ; k++) {
+                    if (s1.charAt(i-1) == s2.charAt(j-1) && s2.charAt(j-1) == s3.charAt(k-1)) {
                         dp[i][j][k] = dp[i-1][j-1][k-1] + 1;
                     } else {
                         dp[i][j][k] = Math.max(dp[i-1][j][k], Math.max(dp[i][j-1][k], dp[i][j][k-1]));
