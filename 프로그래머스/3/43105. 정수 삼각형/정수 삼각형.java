21dbdb95b1124de63c5fc13e7d0fefccd5a87eeb
hyeongseok Lee <gudtjr2949@naver.com>
2024-11-04 12:09:19 +0900

    [level 3] Title: 정수 삼각형, Time: 12.32 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -10,22 +10,20 @@ class Solution {
         
         solve(triangle);
         
-        answer = dp[0][0] + triangle[0][0];
+        answer = dp[0][0];
         
         return answer;
     }
     
-    static void solve (int[][] triangle) {
-        for (int i = N-1 ; i >= 1 ; i--) {
+    static void solve(int[][] triangle) {
+        for (int i = 0 ; i < triangle[N-1].length ; i++)
+            dp[N-1][i] = triangle[N-1][i];
+        
+        for (int i = N-2 ; i >= 0 ; i--) {
             for (int j = 0 ; j < triangle[i].length ; j++) {
-                if (dp[i-1][j] < dp[i][j] + triangle[i][j]) {
-                    dp[i-1][j] = dp[i][j] + triangle[i][j];
-                }
-                
-                if (j-1 >= 0 && dp[i-1][j-1] < dp[i][j] + triangle[i][j]) {
-                    dp[i-1][j-1] = dp[i][j] + triangle[i][j];
-                }
+                dp[i][j] = triangle[i][j] + Math.max(dp[i+1][j], dp[i+1][j+1]);
             }
         }
     }
+    
 }
