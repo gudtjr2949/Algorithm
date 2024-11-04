e123abc08a37e21b8712c99a0d0a97b87f8979a5
hyeongseok Lee <gudtjr2949@naver.com>
2024-11-04 12:45:57 +0900

    [level 3] Title: 등굣길, Time: 0.77 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,25 +1,33 @@
+import java.util.*;
+
 class Solution {
+    
+    static int answer;
+    static int[][] dp;
+    
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
+        dp = new int[n+1][m+1];
         
-        int[][] map = new int[n+1][m+1];
-        int[][] dp = new int[n+1][m+1];
+        for (int i = 0 ; i < puddles.length ; i++) 
+            dp[puddles[i][1]][puddles[i][0]] = -1;
         
-        for (int i = 0 ; i < puddles.length ; i++) {
-            map[puddles[i][1]][puddles[i][0]] = 1;
-        }
-        
+        solve(m, n);
+    
+        return answer;
+    }
+    
+    static void solve(int m, int n) {
         dp[1][1] = 1;
         
         for (int i = 1 ; i <= n ; i++) {
             for (int j = 1 ; j <= m ; j++) {
                 if (i == 1 && j == 1) continue;
                 
-                if (map[i][j] == 1) dp[i][j] = 0;
-                else dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1_000_000_007;
+                if (dp[i][j] == -1) dp[i][j] = 0;
+                else dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1_000_000_007;
             }
         }
         
-        return dp[n][m];
+        answer = dp[n][m];
     }
 }
