8aef367b1d950cf2e930b554947f2d9553c5547a
hyeongseok Lee <gudtjr2949@naver.com>
2024-11-05 12:55:59 +0900

    [level 4] Title: 징검다리, Time: 41.59 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -3,16 +3,16 @@ import java.util.*;
 class Solution {
     
     static int answer;
-    static int[] copiedRocks;
+    static int[] arr;
     
     public int solution(int distance, int[] rocks, int n) {
-        answer = 0;
-        copiedRocks = new int[rocks.length + 1];
-        Arrays.sort(rocks);
-        for (int i = 0 ; i < rocks.length ; i++)
-            copiedRocks[i] = rocks[i];
+        arr = new int[rocks.length+1];
+        for (int i = 0 ; i < rocks.length ; i++) {
+            arr[i] = rocks[i];
+        }
+        arr[rocks.length] = distance;
         
-        copiedRocks[rocks.length] = distance;
+        Arrays.sort(arr);
         
         solve(distance, n);
         
@@ -20,31 +20,33 @@ class Solution {
     }
     
     static void solve(int distance, int n) {
-        int left = 1;
+        int left = 0;
         int right = distance;
         
         while (left <= right) {
             int mid = (left + right) / 2;
             
-            int cnt = 0;
-            int cur = 0;
-            
-            for (int i = 0 ; i < copiedRocks.length ; i++) {
-                int next = copiedRocks[i];
-                
-                if (next - cur < mid) cnt++;
-                else cur = next;
-                
-                if (cnt > n) break;
-            }
-            
-            
-            
-            if (cnt <= n) {
+            if (count(mid) <= n) {
                 answer = mid;
                 left = mid+1;
+            } else {
+                right = mid-1;
             }
-            else right = mid-1;
         }
+    }
+    
+    static int count(int mid) {
+        int cur = 0;
+        int cnt = 0;
+        
+        for (int i = 0 ; i < arr.length ; i++) {
+            int next = arr[i];
+            
+            if (next - cur < mid) cnt++;
+            else cur = next;
+            
+        }
+        
+        return cnt;
     }
 }
