476eca5477caf07598b475bc7de42152d787ba8a
hyeongseok Lee <gudtjr2949@naver.com>
2024-11-07 11:22:28 +0900

    [level 3] Title: 순위, Time: 12.81 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,52 +1,42 @@
 class Solution {
     
+    static int answer;
     static int[][] dp;
     
     public int solution(int n, int[][] results) {
-        int answer = 0;
-        
         dp = new int[n+1][n+1];
         
         for (int i = 0 ; i < results.length ; i++) {
-            int a = results[i][0];
-            int b = results[i][1];
-            
-            dp[a][b] = 1;
-            dp[b][a] = -1;
+            dp[results[i][0]][results[i][1]] = 1;
+            dp[results[i][1]][results[i][0]] = -1;
         }
         
-        answer = solve(n);
+        solve(n);
         
         return answer;
     }
     
-    static int solve(int n) {
+    static void solve(int n) {
         for (int k = 1 ; k <= n ; k++) {
             for (int i = 1 ; i <= n ; i++) {
                 for (int j = 1 ; j <= n ; j++) {
                     if (i != j && dp[i][k] == 1 && dp[k][j] == 1) {
                         dp[i][j] = 1;
                         dp[j][i] = -1;
-                        dp[j][k] = -1;
                         dp[k][i] = -1;
+                        dp[j][k] = -1;
                     }
                 }
             }
         }
         
-        int cnt = 0;
-        
         for (int i = 1 ; i <= n ; i++) {
             int know = 0;
             for (int j = 1 ; j <= n ; j++) {
-                if (dp[i][j] == 1 || dp[i][j] == -1) {
-                    know++;
-                }
+                if (dp[i][j] == 1 || dp[i][j] == -1) know++;
             }
             
-            if (know == n-1) cnt++;
+            if (know == n-1) answer++;
         }
-        
-        return cnt;
     }
 }
