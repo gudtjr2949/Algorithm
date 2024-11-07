e79ee54631ba8ead712d549aaba299b3eb00806e
hyeongseok Lee <gudtjr2949@naver.com>
2024-11-07 11:47:21 +0900

    [level 2] Title: 배달, Time: 5.46 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,18 +1,18 @@
+import java.util.*;
+
 class Solution {
     
-    static int MAX = 500_000;
+    static int answer, MAX = 500_000;
     static int[][] dp;
     
     public int solution(int N, int[][] road, int K) {
-        int answer = 0;
-        
         dp = new int[N+1][N+1];
+        for (int i = 0 ; i <= N ; i++) 
+            Arrays.fill(dp[i], MAX);
         
-        for (int i = 0 ; i <= N ; i++) {
-            for (int j = 0 ; j <= N ; j++)
-                if (i != j) dp[i][j] = MAX;
-        }
-
+        for (int i = 0 ; i <= N ; i++) 
+            dp[i][i] = 0;
+        
         for (int i = 0 ; i < road.length ; i++) {
             int a = road[i][0];
             int b = road[i][1];
@@ -22,22 +22,25 @@ class Solution {
             dp[b][a] = Math.min(dp[b][a], c);
         }
         
-        solve(N);
-
-        for (int i = 1 ; i <= N ; i++) {
-            if (dp[1][i] <= K) answer++;
-        }
+        solve(N, K);
 
         return answer;
     }
     
-    static void solve(int N) {
+    static void solve(int N, int K) {
         for (int k = 1 ; k <= N ; k++) {
             for (int i = 1 ; i <= N ; i++) {
                 for (int j = 1 ; j <= N ; j++) {
-                    if (i != j) dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+                    if (i != j && dp[i][j] > dp[i][k] + dp[k][j]) {
+                        dp[i][j] = dp[i][k] + dp[k][j];
+                    }
                 }
             }
         }
+        
+        for (int i = 1 ; i <= N ; i++) {
+            if (dp[1][i] <= K) answer++;
+        }
     }
+
 }
