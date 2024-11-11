f03c98cd36b9cecccbd2f48cbb9a39e17b3212be
hyeongseok Lee <gudtjr2949@naver.com>
2024-11-11 18:23:41 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 808 ms, Memory: 83548 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,104 +1,76 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
-// 백준 2206 : 벽 부수고 이동하기
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
-// 백준 2206 : 벽 부수고 이동하기
 public class Main {
 
-	static int N;
-	static int M;
-	static int[][] board;
-	static ArrayList<Integer[][]> all_board;
-	static boolean[][][] visited;
-	static int[] nx = { 0, 1, 0, -1 };
-	static int[] ny = { -1, 0, 1, 0 };
+    static int N, M, answer;
+    static int[] dx = {0, 1, 0, -1}, dy = {-1, 0, 1, 0};
+    static int[][] map;
+    static boolean[][][] visited;
+    static class Node {
+        int x, y, destory, cnt;
+        public Node(int x, int y, int destory, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.destory = destory;
+            this.cnt = cnt;
+        }
+    }
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		String nm = bf.readLine();
-		String[] nm_arr = nm.split(" ");
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[2][N][M];
 
-		N = Integer.parseInt(nm_arr[0]);
-		M = Integer.parseInt(nm_arr[1]);
-		board = new int[N][M];
-		visited = new boolean[2][N][M];
-		all_board = new ArrayList<>();
+        for (int i = 0 ; i < N ; i++) {
+            String s = bf.readLine();
+            for (int j = 0 ; j < M ; j++) {
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			String s = bf.readLine();
-			for (int j = 0; j < M; j++) {
-				board[i][j] = s.charAt(j) - '0';
-			}
-		}
-		
-		int answer = bfs(0, 0);
-		
-		System.out.println(answer);
-	}
+        solve();
 
-	private static int bfs(int x, int y) {
-		Queue<Point> Q = new LinkedList<>();
+        if (answer == 0) answer = -1;
 
-		Q.add(new Point(x, y, 1, true));
+        System.out.println(answer);
+    }
 
-		while (!Q.isEmpty()) {
-			int size = Q.size();
+    static void solve() {
+        Queue<Node> PQ = new PriorityQueue<>((o1, o2) -> o1.cnt - o2.cnt);
 
-			for (int i = 0; i < size; i++) {
-				Point p = Q.poll();
+        visited[0][0][0] = true;
+        PQ.add(new Node(0, 0, 0, 1));
 
-				if (p.y == N - 1 && p.x == M - 1) {
-					return p.depth;
-				}
+        while (!PQ.isEmpty()) {
+            Node now = PQ.poll();
 
-				if (p.chance) {
-					visited[0][p.y][p.x] = true;
-				} else {
-					visited[1][p.y][p.x] = true;
-				}
+            if (now.x == M-1 && now.y == N-1) {
+                answer = now.cnt;
+                return;
+            }
 
-				for (int j = 0; j < 4; j++) {
-					int dx = p.x + nx[j];
-					int dy = p.y + ny[j];
-					if (p.chance) {
-						if (!(dx >= M || dx < 0 || dy >= N || dy < 0) && !visited[0][dy][dx]) {
-							if (board[dy][dx] == 0) {
-								visited[0][dy][dx] = true;
-								Q.add(new Point(dx, dy, p.depth + 1, p.chance));
-							} else {
-								visited[1][dy][dx] = true;
-								Q.add(new Point(dx, dy, p.depth + 1, false));
-							}
-						}
-					} else {
-						if (!(dx >= M || dx < 0 || dy >= N || dy < 0) && !visited[1][dy][dx] && board[dy][dx] == 0) {
-							visited[1][dy][dx] = true;
-							Q.add(new Point(dx, dy, p.depth + 1, p.chance));
-						}
-					}
-				}
-			}
-		}
-		return -1;
-	}
+            for (int i = 0 ; i < 4 ; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
 
-	static class Point {
-		int x;
-		int y;
-		int depth;
-		boolean chance;
-
-		public Point(int x, int y, int depth, boolean chance) {
-			this.x = x;
-			this.y = y;
-			this.depth = depth;
-			this.chance = chance;
-		}
-	}
+                if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
+                    if (map[ny][nx] == 0 && !visited[now.destory][ny][nx]) {
+                        visited[now.destory][ny][nx] = true;
+                        PQ.add(new Node(nx, ny, now.destory, now.cnt+1));
+                    } else if (map[ny][nx] == 1 && now.destory == 0 && !visited[1][ny][nx]) {
+                        visited[1][ny][nx] = true;
+                        PQ.add(new Node(nx, ny, 1, now.cnt+1));
+                    }
+                }
+            }
+        }
+    }
 }
