14b9a222a2c43410069c18491dec24bae8639259
hyeongseok Lee <gudtjr2949@naver.com>
2024-11-19 11:30:07 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,22 +1,27 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, answer = Integer.MAX_VALUE;
-    static int[][] dp;
+    static int N, answer;
+    static int[][] arr, dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
-        dp = new int[N+1][3];
 
-        for (int i = 1 ; i <= N ; i++) {
+        arr = new int[N][3];
+        dp = new int[N][3];
+        answer = Integer.MAX_VALUE;
+
+        for (int i = 0 ; i < N ; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
-            dp[i][0] = Integer.parseInt(st.nextToken());
-            dp[i][1] = Integer.parseInt(st.nextToken());
-            dp[i][2] = Integer.parseInt(st.nextToken());
+
+            for (int j = 0 ; j < 3 ; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
         solve();
@@ -25,13 +30,16 @@ public class Main {
     }
 
     static void solve() {
-        for (int i = 1 ; i <= N ; i++) {
-            dp[i][0] = Math.min(dp[i][0] + dp[i-1][1], dp[i][0] + dp[i-1][2]);
-            dp[i][1] = Math.min(dp[i][1] + dp[i-1][0], dp[i][1] + dp[i-1][2]);
-            dp[i][2] = Math.min(dp[i][2] + dp[i-1][0], dp[i][2] + dp[i-1][1]);
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+
+        for (int i = 1 ; i < N ; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + arr[i][2];
         }
 
-        for (int i = 0 ; i < 3 ; i++)
-            answer = Math.min(answer, dp[N][i]);
+        answer = Math.min(dp[N-1][0], Math.min(dp[N-1][1], dp[N-1][2]));
     }
 }
