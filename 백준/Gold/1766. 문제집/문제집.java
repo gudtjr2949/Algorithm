9fe479d992fd36c5a1ecb0a1019dbb6a75f2dcaa
hyeongseok Lee <gudtjr2949@naver.com>
2024-11-20 11:35:52 +0900

    [Gold II] Title: 문제집, Time: 504 ms, Memory: 47752 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -1,33 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    static int N;
+    static int N, M;
     static int[] edge;
     static List<List<Integer>> adj;
-    static StringBuilder sb = new StringBuilder();
+    static StringBuilder sb;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
+        M = Integer.parseInt(st.nextToken());
         edge = new int[N+1];
         adj = new ArrayList<>();
+        sb = new StringBuilder();
+
         for (int i = 0 ; i <= N ; i++) adj.add(new ArrayList<>());
 
         for (int i = 0 ; i < M ; i++) {
             st = new StringTokenizer(bf.readLine());
-            int first = Integer.parseInt(st.nextToken());
-            int later = Integer.parseInt(st.nextToken());
-            edge[later]++;
-            adj.get(first).add(later);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            adj.get(A).add(B);
+            edge[B]++;
         }
 
         solve();
@@ -36,7 +34,7 @@ public class Main {
     }
 
     static void solve() {
-        PriorityQueue<Integer> PQ = new PriorityQueue<>();
+        Queue<Integer> PQ = new PriorityQueue<>();
 
         for (int i = 1 ; i <= N ; i++) {
             if (edge[i] == 0) PQ.add(i);
