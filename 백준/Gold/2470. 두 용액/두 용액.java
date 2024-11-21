98b62ffa73328ebd3c19e42988b53141a2cf3a5f
hyeongseok Lee <gudtjr2949@naver.com>
2024-11-21 12:09:25 +0900

    [Gold V] Title: 두 용액, Time: 356 ms, Memory: 28676 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -6,17 +6,16 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N;
-    static int[] arr;
-    static long[] answer;
+    static int[] arr, answer;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(bf.readLine());
         arr = new int[N];
-        answer = new long[2];
+        answer = new int[2];
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
+
         for (int i = 0 ; i < N ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
@@ -31,16 +30,16 @@ public class Main {
     static void solve() {
         int left = 0;
         int right = N-1;
-
-        long min = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
 
         while (left < right) {
-            long sum = arr[left] + arr[right];
+            int sum = arr[left] + arr[right];
 
             if (min > Math.abs(sum)) {
+                min = Math.abs(sum);
                 answer[0] = arr[left];
                 answer[1] = arr[right];
-                min = Math.abs(sum);
+                if (sum == 0) return;
             }
 
             if (sum > 0) right--;
