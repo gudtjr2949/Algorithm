5450b5a4ca30ef5123356e41dd26c46c8dbceaf8
hyeongseok Lee <gudtjr2949@naver.com>
2024-11-29 11:55:15 +0900

    [Gold V] Title: LCS, Time: 132 ms, Memory: 18324 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -3,28 +3,24 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static int N, M;
-    static int[][] dp;
     static String s1, s2;
+    static int[][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         s1 = bf.readLine();
         s2 = bf.readLine();
 
+        dp = new int[s1.length()+1][s2.length()+1];
+
         solve();
 
-        System.out.println(dp[N][M]);
+        System.out.println(dp[s1.length()][s2.length()]);
     }
 
     static void solve() {
-        N = s1.length();
-        M = s2.length();
-
-        dp = new int[N+1][M+1];
-
-        for (int i = 1 ; i <= N ; i++) {
-            for (int j = 1 ; j <= M ; j++) {
+        for (int i = 1 ; i <= s1.length() ; i++) {
+            for (int j = 1 ; j <= s2.length() ; j++) {
                 if (s1.charAt(i-1) == s2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
                 } else {
@@ -33,4 +29,6 @@ public class Main {
             }
         }
     }
+
+
 }
