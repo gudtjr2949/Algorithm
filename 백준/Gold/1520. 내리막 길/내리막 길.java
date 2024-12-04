d57bdec04d3b0328f0832958930d55ccd66eca30
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-04 16:49:46 +0900

    [Gold III] Title: 내리막 길, Time: 340 ms, Memory: 35880 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -4,21 +4,21 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int M, N;
+    static int N, M, answer;
     static int[] dx = {0, 1, 0, -1}, dy = {-1, 0, 1, 0};
     static int[][] map, dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        map = new int[M][N];
-        dp = new int[M][N];
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        dp = new int[N][M];
 
-        for (int i = 0 ; i < M ; i++) {
+        for (int i = 0 ; i < N ; i++) {
             st = new StringTokenizer(bf.readLine());
-            for (int j = 0 ; j < N ; j++) {
+            for (int j = 0 ; j < M ; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 dp[i][j] = -1;
             }
@@ -28,22 +28,17 @@ public class Main {
     }
 
     static int dfs(int x, int y) {
-        if (x == N-1 && y == M-1) { // 도착지점
-            return 1;
-        }
+        if (x == M-1 && y == N-1) return 1;
+        if (dp[y][x] != -1) return dp[y][x]; // 이미 방문한 적 있음
 
-        if (dp[y][x] != -1) { // 이미 방문한 적 있는 좌표
-            return dp[y][x];
-        } else {
-            dp[y][x] = 0;
-            
-            for (int i = 0 ; i < 4 ; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+        dp[y][x] = 0;
 
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M && map[ny][nx] < map[y][x]) {
-                    dp[y][x] += dfs(nx, ny);
-                }
+        for (int i = 0 ; i < 4 ; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < M && ny >= 0 && ny < N && map[ny][nx] < map[y][x]) {
+                dp[y][x] += dfs(nx, ny);
             }
         }
 
