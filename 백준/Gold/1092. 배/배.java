6a6c3f21456720c47188b1f1dabb3544b792bef0
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-04 11:51:51 +0900

    [Gold V] Title: 배, Time: 2360 ms, Memory: 16468 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -1,68 +1,60 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    static int N, M, answer = 0;
-    static Integer[] crane;
-    static ArrayList<Integer> box;
+    static int N, M, answer;
+    static int[] crane;
+    static List<Integer> box;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(bf.readLine());
+        crane = new int[N];
+
+        int maxCrane = 0;
+
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        crane = new Integer[N];
-
         for (int i = 0 ; i < N ; i++) {
             crane[i] = Integer.parseInt(st.nextToken());
+            maxCrane = Math.max(maxCrane, crane[i]);
         }
 
         M = Integer.parseInt(bf.readLine());
-
         box = new ArrayList<>();
 
+        int maxBox = 0;
         st = new StringTokenizer(bf.readLine());
-
         for (int i = 0 ; i < M ; i++) {
             box.add(Integer.parseInt(st.nextToken()));
+            maxBox = Math.max(maxBox, box.get(i));
         }
 
-        Arrays.sort(crane, Collections.reverseOrder());
-        Collections.sort(box, Collections.reverseOrder());
+        Arrays.sort(crane);
 
-        // 가장 무거운 박스의 무게가 가장 무거운 박스를 들 수 있는 크레인의 제한 무게보다 무거운 경우
-        if (crane[0] < box.get(0)) {
-            System.out.println(-1);
+        if (maxCrane < maxBox) {
+            answer = -1;
         } else {
-            solve();
-            System.out.println(answer);
+            Collections.sort(box, Collections.reverseOrder());
+
+            while (!box.isEmpty()) {
+                answer++;
+                solve();
+            }
         }
+
+        System.out.println(answer);
     }
 
-    // 무거운 박스를 들 수 있는 크레인은 무거운 박스를 우선으로 들어야 함
-    // 박스를 들었다면, 그 박스는 box 리스트에서 remove 해야함
     static void solve() {
-        while (!box.isEmpty()) {
-            int boxIdx = 0;
-            for (int i = 0 ; i < N ; ) {
-                if (boxIdx == box.size())
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = 0 ; j < box.size() ; j++) {
+                if (crane[i] >= box.get(j)) {
+                    box.remove(j);
                     break;
-                
-                 if(box.get(boxIdx) <= crane[i]) {
-                    box.remove(boxIdx);
-                    i++;
-                } else {
-                    boxIdx++;
                 }
             }
-
-            answer++;
         }
     }
 }
