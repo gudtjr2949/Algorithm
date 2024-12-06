b2f171bb3f3235f4f7161e0c091f5be8f477d31a
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-06 13:06:46 +0900

    [Gold V] Title: 강의실, Time: 724 ms, Memory: 56172 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.java
+++ 백준/Gold/1374. 강의실/강의실.java
@@ -4,28 +4,15 @@ import java.util.*;
 
 public class Main {
 
-    static int N, lectureRoom;
-    static Queue<Class> PQ = new PriorityQueue<>((c1, c2) -> {
-        if (c1.end != c2.end)
-            return c1.end - c2.end;
-        else
-            return c1.start - c2.start;
-    });
-    static Class[] classes;
+    static int N, answer = 1;
+    static Node[] lecture;
+    static class Node {
+        int idx, start, end;
 
-    static class Class implements Comparable<Class>{
-        int start, end;
-
-        public Class(int start, int end) {
+        public Node(int idx, int start, int end) {
+            this.idx = idx;
             this.start = start;
             this.end = end;
-        }
-
-        public int compareTo(Class c) {
-            if (this.start != c.start)
-                return this.start - c.start;
-            else
-                return this.end - c.end;
         }
     }
 
@@ -33,63 +20,39 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
 
-        classes  = new Class[N];
+        lecture = new Node[N];
 
         for (int i = 0 ; i < N ; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
 
-            int num = Integer.parseInt(st.nextToken());
+            int idx = Integer.parseInt(st.nextToken());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            classes[i] = new Class(start, end);
+            lecture[i] = new Node(idx, start, end);
         }
+
+        Arrays.sort(lecture, (o1, o2) -> {
+            if (o1.start == o2.start) return o1.end - o2.end;
+            return o1.start - o2.start;
+        });
 
         solve();
 
-        System.out.println(lectureRoom);
+        System.out.println(answer);
     }
 
     static void solve() {
-        // 시작 순으로 정렬
-        Arrays.sort(classes);
+        Queue<Integer> PQ = new PriorityQueue<>();
 
-        // 첫 강의 넣기
-        PQ.add(classes[0]);
-
-        // 생성된 강의실
-        lectureRoom = 1;
-
-        for (int i = 1 ; i < classes.length ; i++) {
-            // 강의중인 강의실이 있는 경우
-            if (!PQ.isEmpty()) {
-                // 현재 진행중인 강의
-                Class c = PQ.peek();
-                int nowStart = c.start;
-                int nowEnd = c.end;
-
-                // 다음 강의
-                int nextStart = classes[i].start;
-                int nextEnd = classes[i].end;
-
-                // 만약에 다음 강의와 겹치지 않는 경우? -> 현재 강의실 재활용
-                // 현재 진행중인 강의 종료
-                if (nowEnd <= nextStart) {
-                    PQ.remove();
-                    PQ.add(new Class(nextStart, nextEnd));
-                }
-                // 다음 강의와 겹치는데 현재 빈 강의실(이미 만들어진 강의실)이 있는 경우 -> PQ.size() < lectureRoom
-                else if (PQ.size() < lectureRoom) {
-                    PQ.add(new Class(nextStart, nextEnd));
-                }
-                // 진행하고 있는 강의가 아직 끝나지 않았는데, 다음 강의가 시작해야하는 경우 -> 강의실 하나 더 개설
-                else {
-                    PQ.add(new Class(nextStart, nextEnd));
-                    lectureRoom++;
-                }
-            } else { // 강의실이 텅 빈 경우
-                PQ.add(new Class(classes[i].start, classes[i].end));
+        for (int i = 0 ; i < N ; i++) {
+            while (!PQ.isEmpty() && PQ.peek() <= lecture[i].start) {
+                PQ.poll();
             }
+
+            PQ.add(lecture[i].end);
+
+            answer = Math.max(answer, PQ.size());
         }
     }
 }
