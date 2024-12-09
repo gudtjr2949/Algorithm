72f75931c933d1e8c7307bc42da525d27ac76dee
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-09 13:13:08 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 164 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,15 +1,17 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int start, end, MAX = 100_001, answer;
+    static int N, K, answer, MAX = 100_001;
+    static int[] dp; // i번째 좌표에 도달할 수 있는 최소시간
     static class Node {
         int idx, time;
+
         public Node(int idx, int time) {
             this.idx = idx;
             this.time = time;
@@ -19,49 +21,53 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        start = Integer.parseInt(st.nextToken());
-        end = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        dp = new int[MAX];
+        Arrays.fill(dp, MAX);
+
         solve();
+
         System.out.println(answer);
     }
 
     static void solve() {
-        PriorityQueue<Node> PQ = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                return o1.time - o2.time;
-            }
-        });
-
-        PQ.add(new Node(start, 0));
-        int[] dist = new int[MAX];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        Queue<Node> PQ = new PriorityQueue<>((o1, o2) -> o1.time - o2.time);
+        dp[N] = 0;
         boolean[] visited = new boolean[MAX];
-        dist[start] = 0;
+        visited[N] = true;
+
+        PQ.add(new Node(N, 0));
 
         while (!PQ.isEmpty()) {
             Node now = PQ.poll();
 
-            if (now.idx == end) {
+            if (now.idx == K) {
                 answer = now.time;
                 return;
             }
 
-            if (visited[now.idx]) continue;
-            visited[now.idx] = true;
-
-            if (now.idx+1 < MAX && !visited[now.idx+1] && dist[now.idx+1] > now.time+1) {
-                dist[now.idx+1] = now.time+1;
-                PQ.add(new Node(now.idx+1, now.time+1));
-            }
-            if (now.idx-1 >= 0 && !visited[now.idx-1] && dist[now.idx-1] > now.time+1) {
-                dist[now.idx-1] = now.time+1;
-                PQ.add(new Node(now.idx-1, now.time+1));
+            int nIdx = now.idx*2;
+            if (nIdx < MAX && dp[nIdx] > now.time) {
+//            if (nIdx < MAX && !visited[nIdx]) {
+                dp[nIdx] = now.time;
+                visited[nIdx] = true;
+                PQ.add(new Node(nIdx, now.time));
             }
 
-            if (now.idx * 2 < MAX && !visited[now.idx*2] && dist[now.idx*2] > now.time) {
-                dist[now.idx*2] = now.time;
-                PQ.add(new Node(now.idx*2, now.time));
+            nIdx = now.idx+1;
+            if (nIdx < MAX && dp[nIdx] > now.time+1) {
+                dp[nIdx] = now.time+1;
+                visited[nIdx] = true;
+                PQ.add(new Node(nIdx, now.time+1));
+            }
+
+            nIdx = now.idx-1;
+            if (nIdx >= 0 && dp[nIdx] > now.time+1) {
+                dp[nIdx] = now.time+1;
+                visited[nIdx] = true;
+                PQ.add(new Node(nIdx, now.time+1));
             }
         }
     }
