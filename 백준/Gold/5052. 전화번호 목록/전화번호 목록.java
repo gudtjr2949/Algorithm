0b3630ad08ccca2b36d701f7a9a1065b65e273f8
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-10 12:23:50 +0900

    [Gold IV] Title: 전화번호 목록, Time: 584 ms, Memory: 33600 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -6,38 +6,37 @@ public class Main {
 
     static int N;
     static String[] arr;
-    static StringBuilder sb;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
 
         int T = Integer.parseInt(bf.readLine());
 
+        StringBuilder sb = new StringBuilder();
+
         while (T-- > 0) {
             N = Integer.parseInt(bf.readLine());
-
             arr = new String[N];
 
             for (int i = 0 ; i < N ; i++) {
                 arr[i] = bf.readLine();
             }
 
-            Arrays.sort(arr);
-
-            if (solve()) sb.append("YES");
-            else sb.append("NO");
-            sb.append("\n");
+            sb.append(solve()).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    static boolean solve() {
-        for (int i = 0 ; i < N-1 ; i++) {
-            if (arr[i+1].startsWith(arr[i])) return false;
+    static String solve() {
+        Arrays.sort(arr);
+
+        for (int i = 1 ; i < N ; i++) {
+            if (arr[i].startsWith(arr[i-1])) {
+                return "NO";
+            }
         }
 
-        return true;
+        return "YES";
     }
 }
