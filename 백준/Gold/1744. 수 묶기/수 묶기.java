3d71553c78a3f7c918af2751326c6e6cff61499c
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-10 12:08:55 +0900

    [Gold IV] Title: 수 묶기, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -1,61 +1,49 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
-    static int N, pCnt, nCnt;
-    static long answer;
-    static long[] arr, positive, negative;
+    static int N, answer;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(bf.readLine());
-        arr = new long[N];
+
+        List<Integer> positive = new ArrayList<>();
+        List<Integer> negative = new ArrayList<>();
 
         for (int i = 0 ; i < N ; i++) {
-            long num = Long.parseLong(bf.readLine());
+            int num = Integer.parseInt(bf.readLine());
 
-            if (num > 1) pCnt++;
-            else if (num <= 0) nCnt++;
-
-            arr[i] = num;
+            if (num > 0) positive.add(num);
+            else negative.add(num);
         }
 
-        Arrays.sort(arr);
+        Collections.sort(positive, Collections.reverseOrder());
+        Collections.sort(negative);
 
-        solve();
+        solve(positive);
+        solve(negative);
 
         System.out.println(answer);
     }
 
-    static void solve() {
-        positive = new long[pCnt];
-        negative = new long[nCnt];
+    static void solve(List<Integer> list) {
+        for (int i = 0 ; i < list.size() ; i++) {
+            if (i+1 >= list.size()) {
+                answer += list.get(i);
+            } else {
+                if (list.get(i) + list.get(i+1) > list.get(i) * list.get(i+1)) {
+                    answer += list.get(i) + list.get(i+1);
+                } else {
+                    answer += list.get(i) * list.get(i+1);
+                }
 
-        int pIdx = 0, nIdx = 0;
-
-        for (int i = 0 ; i < N ; i++) {
-            if (arr[i] > 1) positive[pIdx++] = arr[i];
-            else if (arr[i] == 1) answer++;
-            else negative[nIdx++] = arr[i];
-        }
-
-        for (int i = pCnt-1 ; i >= 0 ; i -= 2) {
-            if (i == 0) {
-                answer += positive[i];
-                break;
+                i++;
             }
-            answer += (positive[i] * positive[i-1]);
-        }
-
-        for (int i = 0 ; i < nCnt ; i += 2) {
-            if (i == nCnt-1) {
-                answer += negative[i];
-                break;
-            }
-            answer += (negative[i] * negative[i+1]);
         }
     }
 }
