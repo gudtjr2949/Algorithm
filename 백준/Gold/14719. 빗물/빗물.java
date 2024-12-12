678f428ebac9fe19199a030a6e51cb13e8efaf9f
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-12 12:38:19 +0900

    [Gold V] Title: 빗물, Time: 120 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,36 +1,43 @@
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-    static int h, w;
-    static int[] height;
- 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        h = scan.nextInt();
-        w = scan.nextInt();
- 
-        height = new int[w];
-        for(int i = 0; i < w; i++) {
-            height[i] = scan.nextInt();
+
+    static int answer, H, W;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        H = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        arr = new int[W];
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0 ; i < W ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
- 
-        int result = 0;
-        for(int i = 1; i < w - 1; i++) {
+
+        solve();
+
+        System.out.println(answer);
+    }
+
+    static void solve() {
+        for (int i = 1 ; i < W-1 ; i++) {
             int left = 0;
+
+            for (int j = 0 ; j < i ; j++) {
+                left = Math.max(left, arr[j]);
+            }
+
             int right = 0;
- 
-            for(int j = 0; j < i; j++) {
-                left = Math.max(height[j], left);
+
+            for (int j = i+1 ; j < W ; j++) {
+                right = Math.max(right, arr[j]);
             }
- 
-            for(int j = i + 1; j < w; j++) {
-                right = Math.max(height[j], right);
-            }
- 
-            if(height[i] < left && height[i] < right) result += Math.min(left, right) - height[i];
+
+            if (arr[i] < left && arr[i] < right) answer += Math.min(left, right) - arr[i];
         }
-        System.out.println(result);
     }
 }
