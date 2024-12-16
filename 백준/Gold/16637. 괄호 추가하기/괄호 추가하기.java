c6fb1c87ac0ca3bd449e114e4a7b2c6b714273ed
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-16 13:06:41 +0900

    [Gold III] Title: 괄호 추가하기, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -1,47 +1,55 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     static int N, answer = Integer.MIN_VALUE;
-    static char[] arr;
+    static List<Integer> nums;
+    static List<Character> operations;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
-        arr = new char[N];
+        nums = new ArrayList<>();
+        operations = new ArrayList<>();
+
         String s = bf.readLine();
 
-        for (int i = 0 ; i < N ; i++)
-            arr[i] = s.charAt(i);
+        for (int i = 0 ; i < s.length() ; i++) {
+            char c = s.charAt(i);
 
-        dfs(2, arr[0] - '0');
+            if (c == '+' || c == '-' || c == '*') {
+                operations.add(c);
+            } else {
+                nums.add(c - '0');
+            }
+        }
+
+        dfs(nums.get(0), 0);
 
         System.out.println(answer);
     }
 
-    static void dfs(int idx, int sum) {
-        if (idx >= N) {
-            answer = Math.max(answer, sum);
+    static void dfs(int result, int idx) {
+        if (idx >= operations.size()) {
+            answer = Math.max(answer, result);
             return;
         }
 
-        // 괄호 X
-        dfs(idx+2, cal(sum, arr[idx] - '0', arr[idx-1]));
 
-        // 괄호 O
-        if (idx+2 < N) {
-            int tmp = cal(arr[idx] - '0', arr[idx+2] - '0', arr[idx+1]);
-            dfs(idx+4, cal(sum, tmp, arr[idx-1]));
+        if (idx + 1 < operations.size()) {
+            int result2 = cal(operations.get(idx+1), nums.get(idx+1), nums.get(idx+2));
+            dfs(cal(operations.get(idx), result, result2), idx+2);
         }
+
+        dfs(cal(operations.get(idx), result, nums.get(idx+1)), idx+1);
     }
 
-    static int cal(int num1, int num2, char operation) {
-        if (operation == '*')
-            return num1 * num2;
-        else if (operation == '+')
-            return num1 + num2;
-        else
-            return num1 - num2;
+    static int cal(char operation, int a, int b) {
+        if (operation == '+') return a + b;
+        else if (operation == '-') return a - b;
+        return a * b;
     }
 }
