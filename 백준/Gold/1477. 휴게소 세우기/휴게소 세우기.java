140b37fa558cfb2b31394683a4ea2872da6a89f8
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-19 13:22:13 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 108 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -5,22 +5,17 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M, L, answer;
+    static int N, M, L, MAX, answer;
     static int[] arr;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         L = Integer.parseInt(st.nextToken());
-
         arr = new int[N+2];
-
         st = new StringTokenizer(bf.readLine());
-
-        arr[0] = 0;
         for (int i = 1 ; i <= N ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
@@ -28,33 +23,36 @@ public class Main {
 
         Arrays.sort(arr);
 
-        solve();
+        binarySearch();
 
         System.out.println(answer);
     }
 
-    static void solve() {
+    static void binarySearch() {
         int left = 1;
-        int right = L-1;
+        int right = L;
 
         while (left < right) {
             int mid = (left + right) / 2;
 
-            if (buildRest(mid) > M) {
-                left = mid+1;
-            } else {
+            // 가장 작은 mid 를 찾아야 함
+            if (buildStation(mid) <= M) {
                 answer = mid;
                 right = mid;
+            } else {
+                left = mid+1;
             }
         }
     }
 
-    static int buildRest(int mid) {
+    static int buildStation(int mid) {
         int cnt = 0;
 
-        for (int i = 1 ; i <= N+1 ; i++) {
-            if ((arr[i] - arr[i-1]) % mid == 0) cnt += ((arr[i] - arr[i-1]) / mid) - 1;
-            else cnt += ((arr[i] - arr[i-1]) / mid);
+        for (int i = 1 ; i < arr.length ; i++) {
+            int dist = arr[i] - arr[i-1];
+            cnt += (dist / mid);
+
+            if (dist % mid == 0) cnt--;
         }
 
         return cnt;
