d223bf8663f0bb02603b9cad78d05322281ee73c
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-20 12:15:16 +0900

    [Gold III] Title: 과제, Time: 120 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.java
+++ 백준/Gold/13904. 과제/과제.java
@@ -1,39 +1,37 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, maxDay, answer;
-    static PriorityQueue<Node> PQ;
+    static int N, MAX, answer;
+    static int[] arr;
+    static Queue<Node> PQ;
     static class Node {
-        int day, weight;
+        int d, w;
 
-        public Node(int day, int weight) {
-            this.day = day;
-            this.weight = weight;
+        public Node(int d, int w) {
+            this.d = d;
+            this.w = w;
         }
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
-        PQ = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                return o2.weight - o1.weight;
-            }
-        });
+        PQ = new PriorityQueue<>((o1, o2) -> o2.w - o1.w);
 
         for (int i = 0 ; i < N ; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
-            int day = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            PQ.add(new Node(day, weight));
-            maxDay = Math.max(maxDay, day);
+            int d = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            MAX = Math.max(MAX, d);
+            PQ.add(new Node(d, w));
         }
+
+        arr = new int[MAX+1];
 
         solve();
 
@@ -41,18 +39,19 @@ public class Main {
     }
 
     static void solve() {
-        boolean[] visited = new boolean[maxDay+1];
-
         while (!PQ.isEmpty()) {
             Node now = PQ.poll();
 
-            for (int i = now.day ; i >= 1 ; i--) {
-                if (!visited[i]) {
-                    answer += now.weight;
-                    visited[i] = true;
+            for (int i = now.d ; i >= 1 ; i--) {
+                if (arr[i] == 0) {
+                    arr[i] = now.w;
                     break;
                 }
             }
+        }
+
+        for (int i = 1 ; i <= MAX ; i++) {
+            answer += arr[i];
         }
     }
 }
