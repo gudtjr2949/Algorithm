e53bf641e142d151bd94f01aac3e1f0f9c908a01
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-23 13:49:15 +0900

    [Silver I] Title: 후보 추천하기, Time: 144 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -1,77 +1,80 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    static int N;
-    static int[] arr;
-    static Picture[] pictures;
-    static class Picture {
-        int num, cnt, time;
+    static int N, M;
+    static int[] answer;
+    static List<Node> list;
+    static Set<Integer> set;
+    static class Node {
+        int time, idx, cnt;
 
-        public Picture(int num, int cnt, int time) {
-            this.num = num;
-            this.cnt = cnt;
+        public Node(int time, int idx, int cnt) {
             this.time = time;
+            this.idx = idx;
+            this.cnt = cnt;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "time=" + time +
+                    ", idx=" + idx +
+                    ", cnt=" + cnt +
+                    '}';
         }
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
-        pictures = new Picture[N];
-        for (int i = 0 ; i < N ; i++)
-            pictures[i] = new Picture(0, 0, 0);
+        M = Integer.parseInt(bf.readLine());
 
-        int M = Integer.parseInt(bf.readLine());
-        arr = new int[M];
+        list = new ArrayList<>();
+        set = new HashSet<>();
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
+
         for (int i = 0 ; i < M ; i++) {
-            solve(Integer.parseInt(st.nextToken()), i);
-            Arrays.sort(pictures, new Comparator<Picture>() {
-                @Override
-                public int compare(Picture o1, Picture o2) {
-                    if (o1.cnt == o2.cnt) return o1.time - o2.time;
-                    else return o1.cnt - o2.cnt;
-                }
-            });
+            solve(i, Integer.parseInt(st.nextToken()));
         }
 
-        Arrays.sort(pictures, new Comparator<Picture>() {
-            @Override
-            public int compare(Picture o1, Picture o2) {
-                return o1.num - o2.num;
-            }
-        });
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0 ; i < N ; i++) {
-            if (pictures[i].num == 0) continue;
-            sb.append(pictures[i].num).append(" ");
+        answer = new int[list.size()];
+        for (int i = 0 ; i < list.size() ; i++) {
+            answer[i] = list.get(i).idx;
         }
 
-        System.out.println(sb);
+        Arrays.sort(answer);
+
+        for (int i = 0 ; i < answer.length ; i++) {
+            System.out.print(answer[i] + " ");
+        }
     }
 
-    static void solve(int now, int time) {
-        for (int i = 0 ; i < N ; i++) {
-            if (pictures[i].num == now) {
-                pictures[i].cnt++;
-                return;
-            }
+    static void solve(int time, int idx) {
+        // 사진틀에서 학생 한명 뺴야함
+        if (!set.contains(idx) && list.size() >= N) {
+            Collections.sort(list, (o1, o2) -> {
+                if (o1.cnt == o2.cnt) return o1.time - o2.time;
+                return o1.cnt - o2.cnt;
+            });
+            set.remove(list.get(0).idx);
+            list.remove(0);
         }
 
-        for (int i = 0 ; i < N ; i++) {
-            if (pictures[i].num == 0) {
-                pictures[i] = new Picture(now, 1, time);
-                return;
+        if (set.contains(idx)) {
+            for (int i = 0 ; i < list.size() ; i++) {
+                if (list.get(i).idx == idx) {
+                    list.get(i).cnt++;
+                    break;
+                }
             }
+        } else {
+            set.add(idx);
+            list.add(new Node(time, idx, 1));
         }
 
-        pictures[0] = new Picture(now, 1, time);
     }
 }
