07f6405ba4ae7f375ecbfd116a0239ad7fef5803
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-23 13:30:30 +0900

    [Gold V] Title: 순열의 순서, Time: 116 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -1,72 +1,71 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, num;
+    static int N, problem;
     static long K;
     static int[] arr;
-    static long[] fac;
+    static long[] factorials;
     static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(bf.readLine());
-        fac = new long[N+1];
-        visited = new boolean[N+1];
+        visited = new boolean[21];
+        factorials = new long[21];
+        arr = new int[N];
 
-        fac[0] = 1;
-        for (int i = 1 ; i <= N ; i++) fac[i] = fac[i-1] * i;
+        factorials[0] = 1;
+        for (int i = 1 ; i <= N ; i++) {
+            factorials[i] = factorials[i-1] * i;
+        }
 
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        num = Integer.parseInt(st.nextToken());
-        if (num == 1) {
+        problem = Integer.parseInt(st.nextToken());
+        if (problem == 1) {
             K = Long.parseLong(st.nextToken());
             solve1();
-        }
-        else {
-            arr = new int[N];
-            for (int i = 0 ; i < N ; i++) arr[i] = Integer.parseInt(st.nextToken());
+        } else {
+            for (int i = 0 ; i < N ; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
             solve2();
         }
     }
 
     static void solve1() {
-        List<Integer> list = new ArrayList<>();
         for (int i = 0 ; i < N ; i++) {
             for (int j = 1 ; j <= N ; j++) {
                 if (visited[j]) continue;
-                if(fac[N-i-1] < K) K -= fac[N-i-1];
-                else {
-                    list.add(j);
+
+                if (factorials[N-i-1] < K) {
+                    K -= factorials[N-i-1];
+                } else {
+                    arr[i] = j;
                     visited[j] = true;
                     break;
                 }
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for (Integer num : list) sb.append(num).append(" ");
-        System.out.println(sb);
+
+        for (int i = 0 ; i < N ; i++) System.out.print(arr[i] + " ");
     }
 
     static void solve2() {
-        long result = 1L;
+        long answer = 1;
 
         for (int i = 0 ; i < N ; i++) {
             for (int j = 1 ; j < arr[i] ; j++) {
-                if (!visited[j]) {
-                    result += fac[N-i-1];
-                }
+                if (visited[j]) continue;
+                answer += factorials[N-i-1];
             }
+
             visited[arr[i]] = true;
         }
 
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
