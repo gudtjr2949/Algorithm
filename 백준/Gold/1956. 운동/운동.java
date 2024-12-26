9fba0beb4b19fa504e4782e6ae05c4b24b93025a
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-26 11:21:44 +0900

    [Gold IV] Title: 운동, Time: 604 ms, Memory: 59656 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -5,56 +5,58 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int V;
-    static int[][] dp;
+    static int V, E;
+    static long answer = Integer.MAX_VALUE;
+    static long[][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
 
-        dp = new int[V+1][V+1];
+        dp = new long[V+1][V+1];
+
         for (int i = 0 ; i <= V ; i++) {
-            for (int j = 0 ; j <= V ; j++) {
-                if (i != j) dp[i][j] = 100_000_000;
-            }
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+            dp[i][i] = 0;
         }
 
         for (int i = 0 ; i < E ; i++) {
             st = new StringTokenizer(bf.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            dp[a][b] = c;
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            dp[from][to] = cost;
         }
 
-        int answer = solve();
+        solve();
+        
+        
+        if (answer == Integer.MAX_VALUE) answer = -1;
 
-        System.out.println(answer == 100_000_000 ? -1 : answer);
+        System.out.println(answer);
     }
 
-    static int solve() {
+    static void solve() {
         for (int k = 1 ; k <= V ; k++) {
             for (int i = 1 ; i <= V ; i++) {
                 for (int j = 1 ; j <= V ; j++) {
-                    if (i != j) {
-                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+                    if (i == j) continue;
+
+                    if (dp[i][k] + dp[k][j] < dp[i][j]) {
+                        dp[i][j] = dp[i][k] + dp[k][j];
                     }
                 }
             }
         }
 
-        int answer = 100_000_000;
-
         for (int i = 1 ; i <= V ; i++) {
             for (int j = 1 ; j <= V ; j++) {
-                if (i != j && (dp[i][j] != Integer.MAX_VALUE && dp[j][i] != Integer.MAX_VALUE)) {
-                    answer = Math.min(answer, dp[i][j] + dp[j][i]);
-                }
+                if (i == j) continue;
+
+                answer = Math.min(answer, dp[i][j] + dp[j][i]);
             }
         }
-
-        return answer;
     }
 }
