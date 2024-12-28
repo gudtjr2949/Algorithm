fcdafc08f34e41432861b9825103f9ab5dd64aea
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-28 19:40:12 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 268 ms, Memory: 29840 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
@@ -6,69 +6,99 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, childA, childB;
-    static boolean[] visited;
+    static int N;
     static List<List<Integer>> tree;
-    static List<Integer> listA, listB;
+    static int[] depth, parents;
+    static boolean[] visited, root;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int Test = Integer.parseInt(bf.readLine());
+        int T = Integer.parseInt(bf.readLine());
 
-        for (int T = 0; T < Test; T++) {
+        while (T-- > 0 ) {
             N = Integer.parseInt(bf.readLine());
 
             tree = new ArrayList<>();
-
-            visited = new boolean[N + 1];
-
-            for (int i = 0; i <= N; i++) {
+            for (int i = 0 ; i <= N ; i++) {
                 tree.add(new ArrayList<>());
             }
+
+            depth = new int[N+1];
+            parents = new int[N+1];
+            visited = new boolean[N+1];
+            root = new boolean[N+1];
 
             for (int i = 0; i < N - 1; i++) {
                 StringTokenizer st = new StringTokenizer(bf.readLine());
 
-                int parents = Integer.parseInt(st.nextToken());
-                int child = Integer.parseInt(st.nextToken());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
 
-                tree.get(child).add(parents);
+                tree.get(a).add(b);
+                tree.get(b).add(a);
+                root[b] = true;
             }
+
+            int rootNode = 0;
+
+            for (int i = 1 ; i <= N ; i++) {
+                if (!root[i]) {
+                    rootNode = i;
+                    break;
+                }
+            }
+
+            visited[rootNode] = true;
+            dfs(rootNode, 0);
 
             StringTokenizer st = new StringTokenizer(bf.readLine());
 
-            childA = Integer.parseInt(st.nextToken());
-            childB = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            listA = new ArrayList<>();
-            listB = new ArrayList<>();
-
-            listA.add(childA);
-            listB.add(childB);
-
-            dfs(childA, listA);
-            dfs(childB, listB);
-
-            Loop:
-            for (Integer A : listA) {
-                for (Integer B : listB) {
-                    if (A.equals(B)) {
-                        sb.append(A).append("\n");
-                        break Loop;
-                    }
-                }
+            // b가 무조건 깊이가 깊음
+            if (depth[a] > depth[b]) {
+                int tmp = a;
+                a = b;
+                b = tmp;
             }
+
+            solve(a, b);
+
         }
 
         System.out.println(sb);
     }
 
-    static void dfs(int child, List<Integer> list) {
-        for (Integer parents : tree.get(child)) {
-            list.add(parents);
-            dfs(parents, list);
+    static void solve(int a, int b) {
+        int depthA = depth[a];
+        int depthB = depth[b];
+
+        if (depthA != depthB) {
+            while (depthA != depthB) {
+                b = parents[b];
+                depthB = depth[b];
+            }
+        }
+
+        while (a != b) {
+            a = parents[a];
+            b = parents[b];
+        }
+
+        sb.append(a).append("\n");
+    }
+
+    static void dfs(int idx, int floor) {
+        for (int next : tree.get(idx)) {
+            if (!visited[next]) {
+                visited[next] = true;
+                depth[next] = floor+1;
+                parents[next] = idx;
+                dfs(next, floor+1);
+            }
         }
     }
 }
