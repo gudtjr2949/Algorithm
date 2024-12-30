7888b29a92363e3fdd32e618d1e7a3db7b670913
hyeongseok Lee <gudtjr2949@naver.com>
2024-12-30 11:38:32 +0900

    [Gold V] Title: AC, Time: 1660 ms, Memory: 84344 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,33 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
-    static String P;
+    static String operation;
+    static List<Integer> list;
     static StringBuilder sb;
-    static Deque<String> deque;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(bf.readLine());
-
         sb = new StringBuilder();
 
         while (T-- > 0) {
-            P = bf.readLine();
+            operation = bf.readLine();
             N = Integer.parseInt(bf.readLine());
-            deque = new ArrayDeque<>();
             String s = bf.readLine();
-            s = s.substring(1, s.length()-1);
-            String[] tmp = s.split(",");
-
-            for (int i = 0 ; i < N ; i++) {
-                deque.add(tmp[i]);
+            s = s.replaceAll("[\\[\\]]", "");
+            StringTokenizer st = new StringTokenizer(s, ",");
+            list = new ArrayList<>();
+            while (st.hasMoreTokens()) {
+                list.add(Integer.parseInt(st.nextToken()));
             }
 
             solve();
@@ -37,31 +34,37 @@ public class Main {
     }
 
     static void solve() {
-        boolean dir = true; // TRUE : 앞에서, FALSE : 뒤에서
-        for (int i = 0 ; i < P.length() ; i++) {
-            if (P.charAt(i) == 'R') {
-                dir = !dir;
+        boolean forward = true;
+
+        for (int i = 0 ; i < operation.length() ; i++) {
+            if (operation.charAt(i) == 'R') {
+                forward = !forward;
             } else {
-                if (deque.isEmpty()) {
+                if (list.size() == 0) {
                     sb.append("error").append("\n");
                     return;
                 }
 
-                if (dir) deque.pollFirst();
-                else deque.pollLast();
+                if (forward) list.remove(0);
+                else list.remove(list.size()-1);
             }
         }
 
         sb.append("[");
-        while (!deque.isEmpty()) {
-            if (dir) {
-                sb.append(deque.pollFirst());
-            } else {
-                sb.append(deque.pollLast());
-            }
 
-            if (deque.size() != 0)
-                sb.append(",");
+        if (forward && list.size() > 0) {
+            // 정방향 : 앞 -> 뒤
+            for (int i = 0 ; i < list.size() ; i++) {
+                sb.append(list.get(i)).append(",");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+
+        } else if (!forward && list.size() > 0) {
+            // 역방향 : 뒤 -> 앞
+            for (int i = list.size()-1 ; i >= 0 ; i--) {
+                sb.append(list.get(i)).append(",");
+            }
+            sb.deleteCharAt(sb.length() - 1);
         }
 
         sb.append("]").append("\n");
