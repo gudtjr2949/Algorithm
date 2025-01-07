176cacdcb8f8051dd8200d2b7da1c0c2df4771e0
hyeongseok Lee <gudtjr2949@naver.com>
2025-01-07 13:07:15 +0900

    [Silver I] Title: 신입 사원, Time: 2980 ms, Memory: 316664 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -8,28 +8,28 @@ public class Main {
     static int N, answer;
     static Node[] arr;
     static class Node {
-        int first, second, rank;
-
-        public Node(int first, int second, int rank) {
+        int first, second;
+        public Node(int first, int second) {
             this.first = first;
             this.second = second;
-            this.rank = rank;
         }
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int T = Integer.parseInt(bf.readLine());
 
         while (T-- > 0) {
-            answer = 1;
             N = Integer.parseInt(bf.readLine());
+            answer = 1;
             arr = new Node[N];
+
             for (int i = 0 ; i < N ; i++) {
                 StringTokenizer st = new StringTokenizer(bf.readLine());
-                arr[i] = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), 0);
+                int first = Integer.parseInt(st.nextToken());
+                int second = Integer.parseInt(st.nextToken());
+                arr[i] = new Node(first, second);
             }
 
             solve();
@@ -43,18 +43,12 @@ public class Main {
     static void solve() {
         Arrays.sort(arr, (o1, o2) -> o1.first - o2.first);
 
-        for (int i = 0 ; i < N ; i++)
-            arr[i].rank = i;
-
-
-        Arrays.sort(arr, (o1, o2) -> o1.second - o2.second);
-
-        int mostHighRank = arr[0].rank;
+        int max = arr[0].second;
 
         for (int i = 1 ; i < N ; i++) {
-            if (arr[i].rank < mostHighRank) {
-                mostHighRank = arr[i].rank;
+            if (arr[i].second < max) {
                 answer++;
+                max = arr[i].second;
             }
         }
     }
