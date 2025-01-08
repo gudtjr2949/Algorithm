5e63c83fa54230e7b01de9650697ba62feee18fe
hyeongseok Lee <gudtjr2949@naver.com>
2025-01-08 11:32:23 +0900

    [Gold IV] Title: 도서관, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.java
+++ 백준/Gold/1461. 도서관/도서관.java
@@ -1,63 +1,55 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
-    static PriorityQueue<Integer> positivePQ = new PriorityQueue<>((o1, o2) -> o2 - o1);
-    static PriorityQueue<Integer> negativePQ = new PriorityQueue<>((o1, o2) -> o2 - o1);
+    static int N, K, answer;
+    static List<Integer> negative, positive;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        negative = new ArrayList<>();
+        positive = new ArrayList<>();
 
         st = new StringTokenizer(bf.readLine());
-
         for (int i = 0 ; i < N ; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if (num < 0) negativePQ.add(num * -1);
-            else positivePQ.add(num);
+            if (num > 0) {
+                positive.add(num);
+            } else {
+                negative.add(num);
+            }
         }
 
-        System.out.println(solve());
+        solve();
+
+        System.out.println(answer);
     }
 
-    static int solve() {
-        int sum = 0;
-        int last = 0;
+    static void solve() {
+        Collections.sort(positive, Collections.reverseOrder());
+        Collections.sort(negative);
 
-        // 가장 멀리가야 하는 경우(절댓값이 가장 큰 경우)를 가장 마지막에 수행해, 다시 원점으로 돌아오지 않는 것이 가장 이득임
-        if (positivePQ.isEmpty()) {
-            sum -= negativePQ.peek();
-        } else if (negativePQ.isEmpty()) {
-            sum -= positivePQ.peek();
-        } else {
-            sum -= Math.max(positivePQ.peek(), negativePQ.peek());
+        int max = 0;
+
+        for (int i = 0 ; i < negative.size() ; i+=K) {
+            answer += (negative.get(i) * -2);
+            max = Math.max(negative.get(i) * -1, max);
         }
 
-        while (!positivePQ.isEmpty()) {
-            int max = positivePQ.poll();
-            for (int i = 0; i < M-1; i++) {
-                if (!positivePQ.isEmpty()) positivePQ.poll();
-            }
-
-            sum += max * 2;
+        for (int i = 0 ; i < positive.size() ; i+=K) {
+            answer += (positive.get(i) * 2);
+            max = Math.max(positive.get(i), max);
         }
 
-        while (!negativePQ.isEmpty()) {
-            int max = negativePQ.poll();
-            for (int i = 0; i < M-1; i++) {
-                if (!negativePQ.isEmpty()) negativePQ.poll();
-            }
-
-            sum += max * 2;
-        }
-
-        return sum;
+        answer -= max;
     }
 }
