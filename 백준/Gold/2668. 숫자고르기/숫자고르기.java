8077adc18d36658ba50983abc1ebd2a8b8816637
hyeongseok Lee <gudtjr2949@naver.com>
2025-01-09 12:29:26 +0900

    [Gold V] Title: 숫자고르기, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,53 +1,45 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-    static int N, start = 0;
-    static boolean[] visited;
+    static int N, start;
     static int[] arr;
-    static List<Integer> list = new ArrayList<>();
+    static boolean[] visited;
+    static List<Integer> list;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
+        arr = new int[N+1];
+        visited = new boolean[N+1];
+        list = new ArrayList<>();
 
-        arr = new int[N];
-        visited = new boolean[N];
-
-        for (int i = 0 ; i < N ; i++) {
-            arr[i] = Integer.parseInt(bf.readLine()) - 1;
+        for (int i = 1 ; i <= N ; i++) {
+            int num = Integer.parseInt(bf.readLine());
+            arr[i] = num;
         }
 
-        for (int i = 0 ; i < N ; i++) {
-            visited[i] = true;
+
+        for (int i = 1 ; i <= N ; i++) {
             start = i;
             dfs(i);
-            visited[i] = false;
         }
 
-        Collections.sort(list);
         System.out.println(list.size());
-        for (Integer num : list) {
-            System.out.println(num + 1);
-        }
+        for (Integer num : list) System.out.println(num);
     }
 
-    static void dfs(int idx) {
-        if (arr[idx] == start) { // 시작한 수와 arr[idx] 와 같음 -> 리스트 담기
-            list.add(arr[idx]);
+    static void dfs(int cur) {
+        if (visited[cur]) {
+            if (cur == start) list.add(start);
             return;
         }
 
-        if (!visited[arr[idx]]) {
-            visited[arr[idx]] = true;
-            dfs(arr[idx]);
-            visited[arr[idx]] = false;
-        }
+        visited[cur] = true;
+        dfs(arr[cur]);
+        visited[cur] = false;
     }
-
 }
