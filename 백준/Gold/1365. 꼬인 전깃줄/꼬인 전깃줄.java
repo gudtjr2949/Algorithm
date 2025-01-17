018b92d640b0f0fa94d48fe78ea531e06eb46bf9
hyeongseok Lee <gudtjr2949@naver.com>
2025-01-17 12:43:24 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 316 ms, Memory: 27996 KB -BaekjoonHub

--- 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.java
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.java
@@ -1,54 +1,63 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
-    static int[] arr, dp;
+    static int[] arr;
+    static List<Integer> list;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(bf.readLine());
-
-        arr = new int[N];
-        dp = new int[N+1];
-
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        for (int i = 0 ; i < N ; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        dp[0] = arr[0];
-
-        int idx = 0;
-        int len = 0;
-
-        for (int i = 0 ; i < N ; i++) {
-            if (dp[len] < arr[i]) {
-                len++;
-                dp[len] = arr[i];
-            } else {
-                idx = binarySearch(0, len, arr[i]);
-                dp[idx] = arr[i];
-            }
-        }
-
-        System.out.println(N - len - 1);
+        input();
+        solve();
+        printAnswer();
     }
 
-    static int binarySearch(int start, int end, int num) {
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (dp[mid] > num) {
-                end = mid;
+    static void solve() {
+        for (int i = 0 ; i < N ; i++) {
+            if (list.isEmpty() || list.get(list.size()-1) < arr[i]) {
+                list.add(arr[i]);
             } else {
-                start = mid + 1;
+                int idx = find(arr[i]);
+                list.set(idx, arr[i]);
             }
         }
-        return end;
+    }
+
+    static int find(int key) {
+        int left = 0;
+        int right = list.size()-1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (list.get(mid) < key) {
+                left = mid+1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        init();
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0 ; i < N ; i++) arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    static void init() {
+        arr = new int[N];
+        list = new ArrayList<>();
+    }
+
+    static void printAnswer() {
+        System.out.println(N - list.size());
     }
 }
