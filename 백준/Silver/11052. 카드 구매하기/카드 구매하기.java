913d0eb767f9aba90114a522cac1f5cb86e05bbd
hyeongseok Lee <gudtjr2949@naver.com>
2025-01-20 12:41:45 +0900

    [Silver I] Title: 카드 구매하기, Time: 120 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,33 +1,40 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
+    static int N, answer;
     static int[] arr, dp;
+
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        System.out.println(answer);
+    }
+
+    static void solve() {
+        for (int i = 1 ; i <= N ; i++) {
+            for (int j = 1 ; j <= i / 2 ; j++) {
+                dp[i] = Math.max(dp[i], dp[j] + dp[i - j]);
+            }
+            answer = Math.max(answer, dp[i]);
+        }
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
-        arr = new int[N+1];
-        dp = new int[N+1];
+        init();
         StringTokenizer st = new StringTokenizer(bf.readLine());
         for (int i = 1 ; i <= N ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             dp[i] = arr[i];
         }
-
-        solve();
-
-        System.out.println(dp[N]);
     }
 
-    static void solve() {
-        for (int i = 1 ; i <= N ; i++) {
-            for (int j = 1 ; j <= i ; j++) {
-                dp[i] = Math.max(dp[i], dp[j] + arr[i-j]);
-            }
-        }
+    static void init() {
+        arr = new int[N+1];
+        dp = new int[N+1];
     }
 }
