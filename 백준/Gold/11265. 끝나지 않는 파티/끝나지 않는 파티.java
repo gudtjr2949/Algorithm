0d4c10e0ffca4b3fa16daa0a7af389c629bf3e8f
hyeongseok Lee <gudtjr2949@naver.com>
2025-01-27 13:25:59 +0900

    [Gold IV] Title: 끝나지 않는 파티, Time: 440 ms, Memory: 26928 KB -BaekjoonHub

--- 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.java
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.java
@@ -5,43 +5,55 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, M;
-    static int[][] dist;
-    static StringBuilder sb = new StringBuilder();
+    static int[][] dp;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+        input1(bf);
+        makeDP();
+        input2(bf);
+        System.out.println(sb);
+    }
 
+    static void input1(BufferedReader bf) throws Exception {
+        StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        dist = new int[N][N];
-
-        for (int i = 0 ; i < N ; i++) {
+        init();
+        for (int i = 1 ; i <= N ; i++) {
             st = new StringTokenizer(bf.readLine());
-            for (int j = 0 ; j < N ; j++) {
-                dist[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1 ; j <= N ; j++) {
+                dp[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+    }
 
-        for(int k = 0 ; k < N ; k++) {
-            for(int i = 0 ; i < N ; i++) {
-                for(int j = 0 ; j < N ; j++){
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+    static void input2(BufferedReader bf) throws Exception {
+        for (int i = 0 ; i < M ; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int time = Integer.parseInt(st.nextToken());
+
+            if (dp[from][to] <= time) sb.append("Enjoy other party").append("\n");
+            else sb.append("Stay here").append("\n");
+        }
+    }
+
+    static void makeDP() {
+        for (int k = 1 ; k <= N ; k++) {
+            for (int i = 1 ; i <= N ; i++) {
+                for (int j = 1 ; j <= N ; j++) {
+                    if (i == j) continue;
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
                 }
             }
         }
+    }
 
-        for (int i = 0 ; i < M ; i++) {
-            st = new StringTokenizer(bf.readLine());
-            int A = Integer.parseInt(st.nextToken())-1;
-            int B = Integer.parseInt(st.nextToken())-1;
-            int C = Integer.parseInt(st.nextToken());
-
-            if (dist[A][B] > C) sb.append("Stay here").append("\n");
-            else sb.append("Enjoy other party").append("\n");
-        }
-
-        System.out.println(sb);
+    static void init() {
+        dp = new int[N+1][N+1];
+        sb = new StringBuilder();
     }
 }
