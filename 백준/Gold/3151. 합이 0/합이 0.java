5b4e1def783b52d25f957e5111c4cdf142a97772
hyeongseok Lee <gudtjr2949@naver.com>
2025-01-29 14:00:00 +0900

    [Gold IV] Title: 합이 0, Time: 440 ms, Memory: 16488 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -10,58 +10,61 @@ public class Main {
     static int[] arr;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
-        arr = new int[N];
-
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-
+        input();
         solve();
-
         System.out.println(answer);
     }
 
     static void solve() {
-        for (int i = 0; i < N - 2; i++) {
-            int left = i + 1;
-            int right = N - 1;
+        Arrays.sort(arr);
+
+        for (int i = 0 ; i < N-2 ; i++) {
+            int left = i+1;
+            int right = N-1;
 
             while (left < right) {
                 int sum = arr[i] + arr[left] + arr[right];
 
-                if (sum > 0) right--;
-                else {
-                    if (sum == 0) {
-
-                        if (arr[left] == arr[right]) {
-                            answer += ((right - left) * (right - left + 1)) / 2;
-                            break;
-                        } else {
-                            int leftCnt = 1;
-                            int rightCnt = 1;
-
-                            while (arr[left] == arr[left + 1]) {
-                                leftCnt++;
-                                left++;
-                            }
-
-                            while (arr[right] == arr[right - 1]) {
-                                rightCnt++;
-                                right--;
-                            }
-
-                            answer += leftCnt * rightCnt;
-                        }
+                if (sum == 0) {
+                    if (arr[left] == arr[right]) {
+                        int distance = right - left + 1;
+                        long cnt = (distance * (distance-1)) / 2;
+                        answer += cnt;
+                        break;
                     }
 
-                    left++;
+                    int origin = arr[left];
+                    int leftCnt = 0;
+                    while (arr[left] == origin) {
+                        leftCnt++;
+                        left++;
+                    }
+
+                    origin = arr[right];
+                    int rightCnt = 0;
+                    while (arr[right] == origin) {
+                        rightCnt++;
+                        right--;
+                    }
+
+                    answer += (leftCnt * rightCnt);
+                } else {
+                    if (sum < 0) left++;
+                    else right--;
                 }
             }
         }
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        init();
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0 ; i < N ; i++) arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    static void init() {
+        arr = new int[N];
     }
 }
