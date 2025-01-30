81b6e7f54fb2955a4650b6836a2680f70ea6a07e
hyeongseok Lee <gudtjr2949@naver.com>
2025-01-30 12:55:42 +0900

    [Gold V] Title: 개똥벌레, Time: 720 ms, Memory: 42488 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -5,55 +5,75 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static long N, H, min, cnt;
+    static int N, H;
+    static int[] prefix;
+    static int[][] arr;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        findAnswer();
+        System.out.println(sb);
+    }
+
+    static void findAnswer() {
+        Arrays.sort(prefix);
+
+        int min = prefix[0];
+        int minCnt = 1;
+
+        for (int i = 1 ; i <= H ; i++) {
+            if (prefix[i] == min) minCnt++;
+            else break;
+        }
+
+        sb.append(min).append(" ").append(minCnt);
+    }
+
+
+    static void solve() {
+        Arrays.sort(arr[0]);
+        Arrays.sort(arr[1]);
+
+        for (int i = 1 ; i <= H ; i++) {
+            prefix[i] = binarySearch(0, i) + binarySearch(1, H-i+1);
+        }
+    }
+
+    static int binarySearch(int idx, int target) {
+        int left = 0;
+        int right = N/2;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (arr[idx][mid] >= target) right = mid;
+            else left = mid+1;
+        }
+
+        return (N / 2) - left;
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        N = Long.parseLong(st.nextToken());
-        H = Long.parseLong(st.nextToken());
-
-        long[] arr1 = new long[(int) N/2];
-        long[] arr2 = new long[(int) N/2];
-
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+        init();
         for (int i = 0 ; i < N ; i++) {
             if (i % 2 == 0) {
-                arr1[i/2] = Long.parseLong(bf.readLine());
+                arr[0][i/2] = Integer.parseInt(bf.readLine());
             } else {
-                arr2[i/2] = Long.parseLong(bf.readLine());
+                arr[1][i/2] = Integer.parseInt(bf.readLine());
             }
         }
-
-        min = Integer.MAX_VALUE;
-
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        solve(arr1, arr2);
-
-        System.out.println(min + " " + cnt);
     }
 
-    static void solve(long[] arr1, long[] arr2) {
-        for (int i = 1 ; i <= H ; i++) {
-            long sum = bs(0, N/2, i, arr1) + bs(0, N/2, H-i+1, arr2);
-
-            if (sum < min) {
-                min = sum;
-                cnt = 1;
-            } else if (sum == min) cnt++;
-        }
-    }
-
-    static long bs(long left, long right, long target, long[] arr) {
-        while (left < right) {
-            long mid = (left + right) / 2;
-
-            if (arr[(int) mid] < target) {
-                left = mid+1;
-            } else right = mid;
-        }
-
-        return arr.length - left;
+    static void init() {
+        arr = new int[2][N/2];
+        prefix = new int[H+1];
+        prefix[0] = N;
+        sb = new StringBuilder();
     }
 }
