b6402d611d476656be266f4c4dc00772ebc3c2e7
hyeongseok Lee <gudtjr2949@naver.com>
2025-01-30 12:03:43 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 744 ms, Memory: 122420 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,48 +4,60 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static int[][] arr, dp;
+    static int N, M;
+    static int[][] map, prefix;
+    static int[][] operation;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        arr = new int[N+1][N+1];
-        dp = new int[N+1][N+1];
-
-        for (int i = 1 ; i <= N ; i++) {
-            st = new StringTokenizer(bf.readLine());
-            for (int j = 1 ; j <= N ; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        makeDP();
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0 ; i < M ; i++) {
-            st = new StringTokenizer(bf.readLine());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-
-            int result = dp[y2][x2] - dp[y2][x1-1] - dp[y1-1][x2] + dp[y1-1][x1-1];
-
-            sb.append(result).append("\n");
-        }
-
+        input();
+        solve();
         System.out.println(sb);
     }
 
-    static void makeDP() {
+    static void solve() {
         for (int i = 1 ; i <= N ; i++) {
             for (int j = 1 ; j <= N ; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i][j];
+                prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + map[i][j];
             }
         }
+
+        for (int i = 0 ; i < M ; i++) {
+            int y1 = operation[i][0];
+            int x1 = operation[i][1];
+            int y2 = operation[i][2];
+            int x2 = operation[i][3];
+            int sum = prefix[y2][x2] - prefix[y1-1][x2] - prefix[y2][x1-1] + prefix[y1-1][x1-1];
+            sb.append(sum).append("\n");
+        }
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        init();
+        for (int i = 1 ; i <= N ; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for (int j = 1 ; j <= N ; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0 ; i < M ; i++) {
+            st = new StringTokenizer(bf.readLine());
+            operation[i][0] = Integer.parseInt(st.nextToken());
+            operation[i][1] = Integer.parseInt(st.nextToken());
+            operation[i][2] = Integer.parseInt(st.nextToken());
+            operation[i][3] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static void init() {
+        map = new int[N+1][N+1];
+        prefix = new int[N+1][N+1];
+        operation = new int[M+1][4];
+        sb = new StringBuilder();
     }
 }
