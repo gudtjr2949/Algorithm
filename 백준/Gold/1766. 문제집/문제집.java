cad51d91818411f7b604075fcc051c626953e50a
hyeongseok Lee <gudtjr2949@naver.com>
2025-02-04 12:23:58 +0900

    [Gold II] Title: 문제집, Time: 424 ms, Memory: 43780 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -5,31 +5,13 @@ import java.util.*;
 public class Main {
 
     static int N, M;
-    static int[] edge;
+    static int[] arr;
     static List<List<Integer>> adj;
     static StringBuilder sb;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        edge = new int[N+1];
-        adj = new ArrayList<>();
-        sb = new StringBuilder();
-
-        for (int i = 0 ; i <= N ; i++) adj.add(new ArrayList<>());
-
-        for (int i = 0 ; i < M ; i++) {
-            st = new StringTokenizer(bf.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            adj.get(A).add(B);
-            edge[B]++;
-        }
-
+        input();
         solve();
-
         System.out.println(sb);
     }
 
@@ -37,7 +19,7 @@ public class Main {
         Queue<Integer> PQ = new PriorityQueue<>();
 
         for (int i = 1 ; i <= N ; i++) {
-            if (edge[i] == 0) PQ.add(i);
+            if (arr[i] == 0) PQ.add(i);
         }
 
         while (!PQ.isEmpty()) {
@@ -46,10 +28,31 @@ public class Main {
             sb.append(now).append(" ");
 
             for (int next : adj.get(now)) {
-                edge[next]--;
-
-                if (edge[next] == 0) PQ.add(next);
+                arr[next]--;
+                if (arr[next] == 0) PQ.add(next);
             }
         }
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        init();
+        for (int i = 0 ; i < M ; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int first = Integer.parseInt(st.nextToken());
+            int after = Integer.parseInt(st.nextToken());
+            adj.get(first).add(after);
+            arr[after]++;
+        }
+    }
+
+    static void init() {
+        arr = new int[N+1];
+        adj = new ArrayList<>();
+        for (int i = 0 ; i <= N ; i++) adj.add(new ArrayList<>());
+        sb = new StringBuilder();
     }
 }
