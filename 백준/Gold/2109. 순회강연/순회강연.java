fb946c3c5d857b4fa2312e51b67cc3e7d3c62b7d
hyeongseok Lee <gudtjr2949@naver.com>
2025-02-04 11:25:32 +0900

    [Gold III] Title: 순회강연, Time: 284 ms, Memory: 20076 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, answer, maxD;
-    static PriorityQueue<Node> PQ;
+    static int N, answer;
+    static boolean[] visited;
+    static Queue<Node> PQ;
     static class Node {
         int p, d;
         public Node(int p, int d) {
@@ -17,42 +18,42 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
-
-        PQ = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                return o2.p - o1.p;
-            }
-        });
-
-        for (int i = 0 ; i < N ; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            int p = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-            PQ.add(new Node(p ,d));
-            maxD = Math.max(maxD, d);
-        }
-
+        input();
         solve();
-
         System.out.println(answer);
     }
 
     static void solve() {
-        boolean[] visited = new boolean[maxD+1];
-
         while (!PQ.isEmpty()) {
             Node now = PQ.poll();
 
             for (int i = now.d ; i >= 1 ; i--) {
                 if (!visited[i]) {
-                    answer += now.p;
                     visited[i] = true;
+                    answer += now.p;
                     break;
                 }
             }
         }
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        init();
+        for (int i = 0 ; i < N ; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int p = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            PQ.add(new Node(p, d));
+        }
+    }
+
+    static void init() {
+        PQ = new PriorityQueue<>((o1, o2) -> {
+            if (o1.p == o2.p) return o1.d - o2.d;
+            return o2.p - o1.p;
+        });
+        visited = new boolean[10_001];
     }
 }
