4981322fc5acb81611e65270ab564a3cfdd5c8b6
hyeongseok Lee <gudtjr2949@naver.com>
2025-02-10 11:33:34 +0900

    [Gold IV] Title: 사이클 게임, Time: 480 ms, Memory: 118936 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -1,67 +1,61 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
+    static int N, M, answer;
+    static boolean possible;
     static int[] parents;
-    static boolean find;
-
 
     public static void main(String[] args) throws Exception {
+        input();
+        System.out.println(answer);
+    }
+
+    static void solve(int a, int b) {
+        if (find(a) == find(b)) {
+            possible = true;
+            return;
+        }
+
+        if (a < b) union(a, b);
+        else union(b, a);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a != b) parents[b] = a;
+    }
+
+    static int find(int num) {
+        if (parents[num] == num) return num;
+        return parents[num] = find(parents[num]);
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        int answer = Integer.MAX_VALUE;
-        parents = new int[N];
-        find = false;
-
-        for (int i = 0 ; i < N ; i++) {
-            parents[i] = i;
-        }
-
+        init();
         for (int i = 0 ; i < M ; i++) {
             st = new StringTokenizer(bf.readLine());
-
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            int small = Math.min(a, b);
-            int big = Math.max(a, b);
-
-            if (union(a, b)) {
-                answer = Math.min(answer, i+1);
+            solve(a, b);
+            if (possible) {
+                answer = i+1;
+                break;
             }
         }
-
-        System.out.println(answer == Integer.MAX_VALUE ? 0 : answer);
     }
 
-    /**
-     * A와 B가 연결되는데, 그 두 점의 부모가 같다? -> 두 점의 연결로 인해 사이클이 만들어짐
-     */
-    static boolean union(int A, int B) {
-        int rootA = findRoot(A);
-        int rootB = findRoot(B);
 
-        if (rootA == rootB) {
-            return true;
-        } else {
-            parents[rootB] = rootA;
-            return false;
-        }
-    }
-
-    static int findRoot(int num) {
-        if (num == parents[num]) {
-            return num;
-        } else {
-            return parents[num] = findRoot(parents[num]);
-        }
+    static void init() {
+        parents = new int[N];
+        for (int i = 0 ; i < N ; i++) parents[i] = i;
     }
 }
