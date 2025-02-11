78696b167a22b3668a8dc0ed316499ac5d0a7f64
hyeongseok Lee <gudtjr2949@naver.com>
2025-02-11 11:27:10 +0900

    [Gold II] Title: 책정리, Time: 396 ms, Memory: 39844 KB -BaekjoonHub

--- 백준/Gold/1818. 책정리/책정리.java
+++ 백준/Gold/1818. 책정리/책정리.java
@@ -1,51 +1,62 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N;
+    static int[] arr;
+    static List<Integer> list;
+
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int[] arr = new int[N];
+        input();
+        solve();
+        System.out.println(N - list.size());
+    }
 
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-
+    static void solve() {
         for (int i = 0 ; i < N ; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            if (list.isEmpty() || list.get(list.size()-1) < arr[i]) {
+                list.add(arr[i]);
+                continue;
+            }
+
+            int idx = bs(arr[i]);
+            list.set(idx, arr[i]);
         }
+    }
 
+    static int bs(int find) {
+        int left = 0;
+        int right = list.size()-1;
 
-        List<Integer> list = new ArrayList<>();
+        while (left < right) {
+            int mid = (left + right) / 2;
 
-        list.add(arr[0]);
-
-        for (int i = 0 ; i < N ; i++) {
-            int num = arr[i];
-            int left = 0;
-            int right = list.size()-1;
-
-            if (list.get(right) < num) {
-                list.add(num);
+            if (list.get(mid) < find) {
+                left = mid+1;
             } else {
-                int idx = 0;
-                while (left <= right) {
-                    int mid = (left + right) / 2;
-
-                    if (list.get(mid) < num) {
-                        left = mid+1;
-                    } else {
-                        idx = mid;
-                        right = mid-1;
-                    }
-                }
-
-                list.set(idx, num);
+                right = mid;
             }
         }
 
-        System.out.println(N-list.size());
+        return left;
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        init();
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static void init() {
+        arr = new int[N];
+        list = new ArrayList<>();
     }
 }
