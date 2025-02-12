9c7dba0a70ecb4ac2c220ed1d9bd2d3126d0cbba
hyeongseok Lee <gudtjr2949@naver.com>
2025-02-12 11:50:34 +0900

    [Gold V] Title: LCS, Time: 124 ms, Memory: 18228 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -4,8 +4,9 @@ import java.io.InputStreamReader;
 public class Main {
 
     static int N, M;
-    static String s1, s2;
+    static String A, B;
     static int[][] dp;
+
 
     public static void main(String[] args) throws Exception {
         input();
@@ -16,18 +17,25 @@ public class Main {
     static void solve() {
         for (int i = 1 ; i <= N ; i++) {
             for (int j = 1 ; j <= M ; j++) {
-                if (s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = dp[i-1][j-1]+1;
-                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                if (A.charAt(i-1) == B.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
     }
 
     static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        s1 = bf.readLine();
-        s2 = bf.readLine();
-        N = s1.length();
-        M = s2.length();
+        A = bf.readLine();
+        B = bf.readLine();
+        N = A.length();
+        M = B.length();
+        init();
+    }
+
+    static void init() {
         dp = new int[N+1][M+1];
     }
 }
