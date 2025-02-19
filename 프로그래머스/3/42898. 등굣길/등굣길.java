c3e652b0a42df00f8025d01da04357905edf3b57
hyeongseok Lee <gudtjr2949@naver.com>
2025-02-19 13:05:45 +0900

    [level 3] Title: 등굣길, Time: 0.74 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -2,32 +2,38 @@ import java.util.*;
 
 class Solution {
     
-    static int answer;
+    static int N, M;
     static int[][] dp;
     
     public int solution(int m, int n, int[][] puddles) {
-        dp = new int[n+1][m+1];
-        
-        for (int i = 0 ; i < puddles.length ; i++) 
-            dp[puddles[i][1]][puddles[i][0]] = -1;
-        
-        solve(m, n);
-    
-        return answer;
+        init(m, n);
+        setDP(puddles);
+        solve();
+        return dp[N][M];
     }
     
-    static void solve(int m, int n) {
+    static void solve() {
         dp[1][1] = 1;
         
-        for (int i = 1 ; i <= n ; i++) {
-            for (int j = 1 ; j <= m ; j++) {
+        for (int i = 1 ; i <= N ; i++) {
+            for (int j = 1 ; j <= M ; j++) {
                 if (i == 1 && j == 1) continue;
                 
                 if (dp[i][j] == -1) dp[i][j] = 0;
                 else dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1_000_000_007;
             }
         }
-        
-        answer = dp[n][m];
+    }
+    
+    static void setDP(int[][] puddles) {
+        for (int[] arr : puddles) {
+            dp[arr[1]][arr[0]] = -1;
+        }
+    }
+    
+    static void init(int m, int n) {
+        N = n;
+        M = m;
+        dp = new int[N+1][M+1];
     }
 }
