996c5a3493df7ea9b376e516dd6c84bb56c0b216
hyeongseok Lee <gudtjr2949@naver.com>
2025-02-21 11:58:12 +0900

    [level 3] Title: 네트워크, Time: 0.39 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,31 +1,60 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     
-    static boolean[] visited;
+    static int answer;
+    static int[] parents;
+    static Set<Integer> set;
     
     static public int solution(int n, int[][] computers) {
-        int answer = 0;
-        visited = new boolean[n];
-        
-        for (int i = 0 ; i < n ; i++) {
-            if (!visited[i]) {
-                answer++;
-                dfs(i, n, computers);
-            }
-        }
-
-        return answer;
+        init(n);
+        set(n);
+        solve(n, computers);
+        findAnswer(n);
+        return set.size();
     }
     
-    static void dfs(int idx, int n, int[][] computers) {
-        
+    static void findAnswer(int n) {
+        for (int i = 1 ; i <= n ; i++) {
+            set.add(parents[i]);
+        }
+    }
+    
+    static void solve(int n, int[][] computers) {
         for (int i = 0 ; i < n ; i++) {
-            if (!visited[i] && idx != i && computers[idx][i] == 1) {
-                visited[i] = true;
-                dfs(i, n, computers);
+            for (int j = 0 ; j < n ; j++) {
+                if (i == j) continue;
+                
+                if (computers[i][j] == 1) {
+                    union(i+1, j+1);
+                }
             }
         }
+        
+        for (int i = 1 ; i <= n ; i++) {
+            parents[i] = find(i);
+        }
+    }
+    
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+                
+        if (a <= b) parents[b] = a;
+        else parents[a] = b;
+    }
+    
+    static int find(int num) {
+        if (parents[num] == num) return num;
+        return parents[num] = find(parents[num]);
+    }
+    
+    static void set(int n) {
+        for (int i = 0 ; i <= n ; i++) parents[i] = i;
+    }
+    
+    static void init(int n) {
+        parents = new int[n+1];
+        set = new HashSet<>();
     }
 }
