10067507fd4b64926e3e4cd9d6d0de88be6b1cef
hyeongseok Lee <gudtjr2949@naver.com>
2025-02-21 11:34:34 +0900

    [level 3] Title: 섬 연결하기, Time: 0.72 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -6,41 +6,44 @@ class Solution {
     static int[] parents;
     
     public int solution(int n, int[][] costs) {
-        answer = 0;
-        parents = new int[n];
-        for (int i = 0 ; i < n ; i++) parents[i] = i;
-
-        Arrays.sort(costs, new Comparator<>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[2] - o2[2];
-            }
-        });
-        
-        for (int i = 0 ; i < costs.length ; i++) {
-            int x = costs[i][0];
-            int y = costs[i][1];
-            int cost = costs[i][2];
-            
-            if (findParent(x) != findParent(y)) {
-                union(x, y);
-                answer += cost;
-            }
-        }
-        
+        init(n);
+        set(n);
+        solve(costs);
         return answer;
     }
     
-    static void union(int x, int y) {
-        x = findParent(x);
-        y = findParent(y);
+    static void solve(int[][] costs) {
+        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
         
-        if (x != y) parents[x] = y;
+        for (int[] bridge : costs) {
+            int a = bridge[0];
+            int b = bridge[1];
+            int cost = bridge[2];
+            
+            if (find(a) != find(b)) {
+                union(a, b);
+                answer += cost;
+            }
+        }
     }
     
+    static int find(int num) {
+        if (parents[num] == num) return num;
+        return parents[num] = find(parents[num]);
+    }
     
-    static int findParent(int num) {
-        if (num != parents[num]) return findParent(parents[num]);
-        return num;
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        
+        if (a != b) parents[b] = a;
+    }
+    
+    static void set(int n) {
+        for (int i = 0 ; i < n ; i++) parents[i] = i;
+    }
+    
+    static void init(int n) {
+        parents = new int[n];
     }
 }
