f5dc2d01ec0646ca2c4853d6285aa4b64c49db5f
hyeongseok Lee <gudtjr2949@naver.com>
2025-02-25 12:58:12 +0900

    [Gold II] Title: 친구 네트워크, Time: 580 ms, Memory: 58544 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -7,71 +7,85 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int F;
-    static int[] parents, rank;
-    static int[][] arr;
+    static int idx, F;
+    static int[] parents, friends;
+    static int[][] operations;
     static Map<String, Integer> map;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(bf.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        while (T-- > 0) {
-            F = Integer.parseInt(bf.readLine());
-
-            arr = new int[F][2];
-
-            map = new HashMap<>();
-
-            int idx = 1;
-
-            for (int i = 0 ; i < F ; i++) {
-                StringTokenizer st = new StringTokenizer(bf.readLine());
-                String a = st.nextToken();
-                String b = st.nextToken();
-                if (!map.containsKey(a)) map.put(a, idx++);
-                if (!map.containsKey(b)) map.put(b, idx++);
-
-                arr[i][0] = map.get(a);
-                arr[i][1] = map.get(b);
-            }
-
-            parents = new int[idx];
-            rank = new int[idx];
-
-            for (int i = 0 ; i < idx ; i++) parents[i] = i;
-            Arrays.fill(rank, 1);
-
-            for (int i = 0 ; i < F ; i++) {
-                sb.append(union(arr[i][0], arr[i][1])).append("\n");
-            }
-        }
-
+        input();
         System.out.println(sb);
     }
 
-    static int union(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        if (a != b) {
-            if (a > b) {
-                parents[a] = b;
-                rank[b] += rank[a];
-                return rank[b];
-            } else {
-                parents[b] = a;
-                rank[a] += rank[b];
-                return rank[a];
-            }
+    static void solve() {
+        for (int[] operation : operations) {
+            sb.append(union(operation[0], operation[1])).append("\n");
         }
-
-        return rank[a];
     }
 
     static int find(int num) {
         if (parents[num] == num) return num;
         return parents[num] = find(parents[num]);
+    }
+
+    static int union(int A, int B) {
+        A = find(A);
+        B = find(B);
+
+        if (A == B) return friends[A];
+
+        if (A > B) {
+            parents[A] = B;
+            friends[B] += friends[A];
+            return friends[B];
+        }
+        else {
+            parents[B] = A;
+            friends[A] += friends[B];
+            return friends[A];
+        }
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bf.readLine());
+
+        while (T-- > 0) {
+            F = Integer.parseInt(bf.readLine());
+            init();
+            for (int i = 0; i < F; i++) {
+                StringTokenizer st = new StringTokenizer(bf.readLine());
+
+                String A = st.nextToken();
+                if (!map.containsKey(A)) {
+                    map.put(A, idx++);
+                }
+
+                String B = st.nextToken();
+                if (!map.containsKey(B)) {
+                    map.put(B, idx++);
+                }
+
+                operations[i][0] = map.get(A);
+                operations[i][1] = map.get(B);
+            }
+
+            solve();
+        }
+    }
+
+    static void init() {
+        idx = 0;
+
+        parents = new int[F*2+1];
+        for (int i = 0 ; i <= F*2 ; i++) parents[i] = i;
+
+        friends = new int[F*2+1];
+        Arrays.fill(friends, 1);
+
+        operations = new int[F][2];
+
+        map = new HashMap<>();
     }
 }
