be638800ef2a8c6188d72f2837101b7aefb2a867
hyeongseok Lee <gudtjr2949@naver.com>
2025-03-11 11:46:22 +0900

    [level 2] Title: 가장 큰 수, Time: 3298.87 ms, Memory: 397 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,22 +1,41 @@
 import java.util.*;
 
 class Solution {
+    
+    static int N;
+    static String answer;
+    static String[] arr;
+    
     public String solution(int[] numbers) {
-        String answer = "";
+        answer = "";
         
-        String[] numbersArr = new String[numbers.length];
-        for (int i = 0 ; i < numbers.length ; i++) {
-            numbersArr[i] = String.valueOf(numbers[i]);
-        }
-        
-        Arrays.sort(numbersArr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-        
-        if (numbersArr[0].equals("0")) return "0";
-        
-        for (int i = 0 ; i < numbers.length ; i++) {
-            answer += numbersArr[i];
-        }
-                
+        init(numbers);
+        input(numbers);
+        solve();
+     
         return answer;
+    }
+    
+    static void solve() {
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        if (arr[0].equals("0")) {
+            answer = "0";
+            return;
+        }
+        
+        for (String number : arr) {
+            answer += number;
+        }
+    }
+    
+    static void init(int[] numbers) {
+        N = numbers.length;
+        arr = new String[N];
+    }
+    
+    static void input(int[] numbers) {
+        for (int i = 0 ; i < N ; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
     }
 }
