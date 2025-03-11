41e1ccd1eaf5d996b71b60cd7c779880c6192006
hyeongseok Lee <gudtjr2949@naver.com>
2025-03-11 12:05:30 +0900

    [level 3] Title: 단어 변환, Time: 2.26 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,26 +2,21 @@ import java.util.*;
 
 class Solution {
     
-    static int answer = 0;
-    static Node[] nodeWords;
-    static boolean[] visited;
+    static int answer;
+    static Set<String> set;
     static class Node {
-        int idx, cnt;
+        int idx;
         String s;
-        public Node(int idx, int cnt, String s) {
+        public Node(int idx, String s) {
             this.idx = idx;
-            this.cnt = cnt;
             this.s = s;
         }
     }
     
     public int solution(String begin, String target, String[] words) {
+        answer = 0;
         
-        nodeWords = new Node[words.length+1];
-        visited = new boolean[words.length+1];
-        for (int i = 1 ; i <= words.length ; i++) {
-            nodeWords[i] = new Node(i, 0, words[i-1]);
-        }
+        makeSet(words);
         
         bfs(begin, target);
         
@@ -29,35 +24,46 @@ class Solution {
     }
     
     static void bfs(String begin, String target) {
-        Queue<Node> Q = new LinkedList<>();
-        Q.add(new Node(0, 0, begin));
-        visited[0] = true;
+        Queue<Node> PQ = new PriorityQueue<>((o1, o2) -> o1.idx - o2.idx);
         
-        while (!Q.isEmpty()) {
-            Node now = Q.poll();
+        PQ.add(new Node(0, begin));
+        
+        while (!PQ.isEmpty()) {
+            Node now = PQ.poll();
             
             if (now.s.equals(target)) {
-                answer = now.cnt;
+                answer = now.idx;
                 return;
             }
             
-            for (int i = 1 ; i < nodeWords.length ; i++) {
-                if (!visited[i] && possibleToChange(now.s, nodeWords[i].s)) {
-                    visited[i] = true;
-                    Q.add(new Node(i, now.cnt+1, nodeWords[i].s));
+            set.remove(now.s);
+            
+            for (String s : set) {
+                if (check(now.s, s)) {
+                    PQ.add(new Node(now.idx+1, s));
                 }
             }
-        }
+        } 
     }
     
-    static boolean possibleToChange(String s1, String s2) {
+    // from -> to 가능한지 확인
+    static boolean check(String from, String to) {
+        if (from.length() != to.length()) return false;
+        
         int cnt = 0;
         
-        for (int i = 0 ; i < s1.length() ; i++) {
-            if (s1.charAt(i) != s2.charAt(i)) cnt++;
+        for (int i = 0 ; i < from.length() ; i++) {
+            if (from.charAt(i) != to.charAt(i)) cnt++;
         }
         
-        if (cnt > 1 || cnt == 0) return false;
-        return true;
+        if (cnt == 1) return true;
+        return false;
+    }
+    
+    static void makeSet(String[] words) {
+        set = new HashSet<>();
+        for (String word : words) {
+            set.add(word);
+        }
     }
 }
