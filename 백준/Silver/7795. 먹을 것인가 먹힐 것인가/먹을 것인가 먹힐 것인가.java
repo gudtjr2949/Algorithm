5b943f492207672576866e618396cd793e43e6bf
hyeongseok Lee <gudtjr2949@naver.com>
2025-03-12 13:25:49 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 416 ms, Memory: 38920 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -5,59 +5,66 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int N, M;
     static int[] A, B;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(bf.readLine());
-
         while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            A = new int[Integer.parseInt(st.nextToken())];
-            B = new int[Integer.parseInt(st.nextToken())];
-
-            st = new StringTokenizer(bf.readLine());
-            for (int i = 0; i < A.length; i++) {
-                A[i] = Integer.parseInt(st.nextToken());
-            }
-
-            st = new StringTokenizer(bf.readLine());
-            for (int i = 0; i < B.length; i++) {
-                B[i] = Integer.parseInt(st.nextToken());
-            }
-
-            Arrays.sort(A);
-            Arrays.sort(B);
-
-            int sum = 0;
-
-            for (int i = 0 ; i < A.length ; i++) {
-                int left = lower(A[i]);
-                sum += left;
-            }
-
-            sb.append(sum).append("\n");
+            input(bf);
+            solve();
         }
 
         System.out.println(sb);
     }
 
-    // 찾고자 하는 값의 최초 위치
-    static int lower(int num) {
+    static void solve() {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int cnt = 0;
+
+        for (int num : A) {
+            cnt += bs(num);
+        }
+
+        sb.append(cnt).append("\n");
+    }
+
+
+    static int bs(int find) {
         int left = 0;
-        int right = B.length;
+        int right = M;
 
         while (left < right) {
             int mid = (left + right) / 2;
 
-            if (B[mid] < num) {
+            if (B[mid] < find) {
                 left = mid+1;
             } else {
                 right = mid;
             }
         }
 
-        return right;
+        return left;
+    }
+
+    static void input(BufferedReader bf) throws Exception {
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        init();
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0 ; i < N ; i++) A[i] = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0 ; i < M ; i++) B[i] = Integer.parseInt(st.nextToken());
+    }
+
+    static void init() {
+        A = new int[N];
+        B = new int[M];
     }
 }
