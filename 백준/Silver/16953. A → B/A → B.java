6bec5d1df566c05d4c0fa3689451c5f48df51ca5
hyeongseok Lee <gudtjr2949@naver.com>
2025-03-13 11:49:55 +0900

    [Silver II] Title: A → B, Time: 108 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,65 +1,61 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    static int A, B, answer;
+    static int A, B, answer = -1;
+    static Set<Integer> set;
     static class Node {
-        String num;
-        int cnt;
+        int num, cnt;
 
-        public Node(String num, int cnt) {
+        public Node(int num, int cnt) {
             this.num = num;
             this.cnt = cnt;
         }
     }
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        System.out.println(answer);
+    }
+
+    static void solve() {
+        Queue<Node> PQ = new PriorityQueue<>((o1, o2) -> o1.cnt - o2.cnt);
+        PQ.add(new Node(B, 1));
+        set.add(B);
+
+        while (!PQ.isEmpty()) {
+            Node now = PQ.poll();
+            int num = now.num;
+
+            if (num == A) {
+                answer = now.cnt;
+                return;
+            }
+
+            if (num % 10 == 1 && !set.contains(num / 10)) {
+                set.add(num / 10);
+                PQ.add(new Node(num / 10, now.cnt + 1));
+            }
+
+            if (num % 2 == 0 && !set.contains(num / 2)) {
+                set.add(num / 2);
+                PQ.add(new Node(num / 2, now.cnt + 1));
+            }
+        }
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
-
-        bfs();
-
-        System.out.println(answer);
+        init();
     }
 
-    private static void bfs() {
-        PriorityQueue<Node> PQ = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node n1, Node n2) {
-                return n1.cnt - n2.cnt;
-            }
-        });
-
-        PQ.add(new Node(Integer.toString(B), 0));
-
-        while (!PQ.isEmpty()) {
-            Node now = PQ.poll();
-            int num = Integer.parseInt(now.num);
-
-            if (num < A) {
-                break;
-            }
-
-            if (num == A) {
-                answer = now.cnt+1;
-                return;
-            }
-
-            if (num % 2 == 0) {
-                PQ.add(new Node(Integer.toString(num / 2), now.cnt+1));
-            }
-            if (num % 10 == 1) {
-                PQ.add(new Node(Integer.toString(num / 10), now.cnt+1));
-            }
-        }
-
-        answer = -1;
+    static void init() {
+        set = new HashSet<>();
     }
 }
