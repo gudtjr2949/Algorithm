f542842421900cb183ac973a5d29ae86fd10c781
hyeongseok Lee <gudtjr2949@naver.com>
2025-03-14 11:32:18 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 152 ms, Memory: 15236 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -1,41 +1,48 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, M;
-    static PriorityQueue<Long> PQ;
+    static long answer;
+    static Queue<Long> PQ;
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        System.out.println(answer);
+    }
+
+    static void solve() {
+        for (int i = 0 ; i < M ; i++) {
+            long a = PQ.poll();
+            long b = PQ.poll();
+            long sum = a+b;
+            PQ.add(sum);
+            PQ.add(sum);
+        }
+
+        while (!PQ.isEmpty()) {
+            answer += PQ.poll();
+        }
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        PQ = new PriorityQueue<>();
-
+        init();
         st = new StringTokenizer(bf.readLine());
-
-        for (int i = 0 ; i < N ; i++) PQ.add(Long.parseLong(st.nextToken()));
-
-        System.out.println(solve());
+        for (int i = 0 ; i < N ; i++) {
+            PQ.add(Long.parseLong(st.nextToken()));
+        }
     }
 
-    static long solve() {
-        for (int i = 0 ; i < M ; i++) {
-            long a = PQ.poll();
-            long b = PQ.poll();
-            PQ.add(a+b);
-            PQ.add(a+b);
-        }
-
-        long sum = 0;
-
-        for (int i = 0 ; i < N ; i++) sum += PQ.poll();
-
-        return sum;
+    static void init() {
+        PQ = new PriorityQueue<>();
     }
 }
