5108099c05db907d0d69a7f95702c0b3719dbc19
hyeongseok Lee <gudtjr2949@naver.com>
2025-03-27 11:13:11 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 504 ms, Memory: 53992 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -7,42 +7,24 @@ public class Main {
     static int V, E;
     static long answer;
     static boolean[] visited;
-    static List<List<Node>> tree;
+    static List<List<Node>> adj;
     static class Node {
-        int idx, cost;
-
-        public Node(int idx, int cost) {
+        int idx;
+        long cost;
+        public Node(int idx, long cost) {
             this.idx = idx;
             this.cost = cost;
         }
     }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        tree = new ArrayList<>();
-        for (int i = 0 ; i <= V ; i++) tree.add(new ArrayList<>());
-        visited = new boolean[V+1];
-
-        for (int i = 0 ; i < E ; i++) {
-            st = new StringTokenizer(bf.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            tree.get(from).add(new Node(to, cost));
-            tree.get(to).add(new Node(from, cost));
-        }
-
+        input();
         solve();
-
         System.out.println(answer);
     }
 
     static void solve() {
-        Queue<Node> PQ = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
-
+        Queue<Node> PQ = new PriorityQueue<>((o1, o2) -> (int) o1.cost - (int) o2.cost);
         PQ.add(new Node(1, 0));
 
         while (!PQ.isEmpty()) {
@@ -52,11 +34,33 @@ public class Main {
             visited[now.idx] = true;
             answer += now.cost;
 
-            for (Node next : tree.get(now.idx)) {
+            for (Node next : adj.get(now.idx)) {
                 if (!visited[next.idx]) {
                     PQ.add(next);
                 }
             }
         }
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        init();
+        for (int i = 0 ; i < E ; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            adj.get(from).add(new Node(to, cost));
+            adj.get(to).add(new Node(from, cost));
+        }
+    }
+
+    static void init() {
+        adj = new ArrayList<>();
+        for (int i = 0 ; i <= V ; i++) adj.add(new ArrayList<>());
+        visited = new boolean[V+1];
     }
 }
