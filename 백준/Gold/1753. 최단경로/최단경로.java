ca040e079ebd9a5ea3e2c7d7e140d94f6fcc93e0
hyeongseok Lee <gudtjr2949@naver.com>
2025-03-28 11:42:43 +0900

    [Gold IV] Title: 최단경로, Time: 856 ms, Memory: 111744 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -4,11 +4,12 @@ import java.util.*;
 
 public class Main {
 
-    static int V, K;
-    static int[] dist;
+    static int V, E, K, MAX = 100_000_000;
+    static int[] dp;
     static List<List<Node>> adj;
     static class Node {
         int idx, cost;
+
         public Node(int idx, int cost) {
             this.idx = idx;
             this.cost = cost;
@@ -16,17 +17,42 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        printAnswer();
+    }
+
+    static void solve() {
+        Queue<Node> PQ = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+        PQ.add(new Node(K, 0));
+        dp[K] = 0;
+
+        while (!PQ.isEmpty()) {
+            Node now = PQ.poll();
+
+            for (Node next : adj.get(now.idx)) {
+                if (dp[next.idx] > dp[now.idx] + next.cost) {
+                    dp[next.idx] = dp[now.idx] + next.cost;
+                    PQ.add(new Node(next.idx, dp[next.idx]));
+                }
+            }
+        }
+    }
+
+    static void printAnswer() {
+        for (int i = 1 ; i <= V ; i++) {
+            if (dp[i] == MAX) System.out.println("INF");
+            else System.out.println(dp[i]);
+        }
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        dist = new int[V+1];
-        adj = new ArrayList<>();
-        for (int i = 0 ; i <= V ; i++)
-            adj.add(new ArrayList<>());
-
+        E = Integer.parseInt(st.nextToken());
+        init();
         K = Integer.parseInt(bf.readLine());
-
         for (int i = 0 ; i < E ; i++) {
             st = new StringTokenizer(bf.readLine());
             int from = Integer.parseInt(st.nextToken());
@@ -34,41 +60,14 @@ public class Main {
             int cost = Integer.parseInt(st.nextToken());
             adj.get(from).add(new Node(to, cost));
         }
-
-        Arrays.fill(dist, Integer.MAX_VALUE);
-
-        solve();
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 1 ; i <= V ; i++) {
-            if (dist[i] == Integer.MAX_VALUE) sb.append("INF").append("\n");
-            else sb.append(dist[i]).append("\n");
-        }
-
-        System.out.println(sb);
     }
 
-    static void solve() {
-        PriorityQueue<Node> PQ = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                return o1.cost - o2.cost;
-            }
-        });
-        
-        PQ.add(new Node(K, 0));
-        dist[K] = 0;
-
-        while (!PQ.isEmpty()) {
-            Node now = PQ.poll();
-
-            for (Node next : adj.get(now.idx)) {
-                if (dist[next.idx] > dist[now.idx] + next.cost) {
-                    dist[next.idx] = dist[now.idx] + next.cost;
-                    PQ.add(new Node(next.idx, dist[next.idx]));
-                }
-            }
+    static void init() {
+        adj = new ArrayList<>();
+        for (int i = 0 ; i <= V ; i++) {
+            adj.add(new ArrayList<>());
         }
+        dp = new int[V+1];
+        Arrays.fill(dp, MAX);
     }
 }
