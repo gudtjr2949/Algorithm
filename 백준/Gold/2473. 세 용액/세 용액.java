c08da8134e2327b1a888f5752016e57065599fea
hyeongseok Lee <gudtjr2949@naver.com>
2025-03-31 12:16:07 +0900

    [Gold III] Title: 세 용액, Time: 208 ms, Memory: 17352 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -5,52 +5,62 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
+    static int N, idx;
+    static long min;
+    static boolean find;
     static long[] arr, answer;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
-        arr = new long[N];
-        answer = new long[3];
-
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        for (int i = 0 ; i < N ; i++)
-            arr[i] = Long.parseLong(st.nextToken());
-
-
-        Arrays.sort(arr);
-
+        input();
         solve();
-
         System.out.println(answer[0] + " " + answer[1] + " " + answer[2]);
     }
 
     static void solve() {
-        long min = Long.MAX_VALUE;
+        Arrays.sort(arr);
 
         for (int i = 0 ; i < N-2 ; i++) {
-            int left = i+1;
-            int right = N-1;
+            idx = i;
+            bs(i+1, N-1);
+            if (find) break;
+        }
+    }
 
-            while (left < right) {
-                long sum = arr[i] + arr[left] + arr[right];
+    static void bs(int left, int right) {
+        while (left < right) {
+            long sum = arr[idx] + arr[left] + arr[right];
 
-                if (min > Math.abs(sum)) {
-                    answer[0] = arr[i];
-                    answer[1] = arr[left];
-                    answer[2] = arr[right];
-                    min = Math.abs(sum);
-                }
+            if (Math.abs(sum) < min) {
+                min = Math.abs(sum);
 
+                answer[0] = arr[idx];
+                answer[1] = arr[left];
+                answer[2] = arr[right];
 
-                if (sum < 0) {
-                    left++;
-                } else {
-                    if (min == 0) return;
-                    right--;
+                if (min == 0L) {
+                    find = true;
+                    return;
                 }
             }
+
+            if (sum < 0) left++;
+            else right--;
         }
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        init();
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0 ; i < N ; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+    }
+
+    static void init() {
+        arr = new long[N];
+        answer = new long[3];
+        min = 3_000_000_001L;
     }
 }
