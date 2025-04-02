4c5f635676e1d87c21bfdc34d11162d1167a9547
hyeongseok Lee <gudtjr2949@naver.com>
2025-04-02 11:28:31 +0900

    [Gold V] Title: 내려가기, Time: 404 ms, Memory: 53412 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -4,66 +4,60 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static int[][] map;
-    static int[][][] dp;
-    static StringBuilder sb;
+    static int N, min, max;
+    static int[][] map, maxDP, minDP;
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        System.out.println(max + " " + min);
+    }
+
+    static void solve() {
+        for (int i = 0 ; i < 3 ; i++) {
+            maxDP[0][i] = map[0][i];
+            minDP[0][i] = map[0][i];
+        }
+
+        for (int i = 1 ; i < N ; i++) {
+            for (int j = 0 ; j < 3 ; j++) {
+                if (j == 0) {
+                    maxDP[i][j] = Math.max(maxDP[i-1][0], maxDP[i-1][1]) + map[i][j];
+                    minDP[i][j] = Math.min(minDP[i-1][0], minDP[i-1][1]) + map[i][j];
+                } else if (j == 1) {
+                    maxDP[i][j] = Math.max(maxDP[i-1][0], Math.max(maxDP[i-1][1], maxDP[i-1][2])) + map[i][j];
+                    minDP[i][j] = Math.min(minDP[i-1][0], Math.min(minDP[i-1][1], minDP[i-1][2])) + map[i][j];
+                } else {
+                    maxDP[i][j] = Math.max(maxDP[i-1][1], maxDP[i-1][2]) + map[i][j];
+                    minDP[i][j] = Math.min(minDP[i-1][1], minDP[i-1][2]) + map[i][j];
+                }
+            }
+        }
+
+        min = Integer.MAX_VALUE;
+        max = 0;
+
+        for (int i = 0 ; i < 3 ; i++) {
+            min = Math.min(min, minDP[N-1][i]);
+            max = Math.max(max, maxDP[N-1][i]);
+        }
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
-
-        sb = new StringBuilder();
-        map = new int[N][3];
-        dp = new int[2][N][3];
-
+        init();
         for (int i = 0 ; i < N ; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             for (int j = 0 ; j < 3 ; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        dp[0][0][0] = map[0][0];
-        dp[0][0][1] = map[0][1];
-        dp[0][0][2] = map[0][2];
-        dp[1][0][0] = map[0][0];
-        dp[1][0][1] = map[0][1];
-        dp[1][0][2] = map[0][2];
-
-        solve();
-
-        System.out.println(sb);
     }
 
-    static void solve() {
-        for (int i = 1 ; i < N ; i++) {
-            for (int j = 0 ; j < 3 ; j++) {
-                switch (j) {
-                    case 0 :
-                        dp[0][i][j] = map[i][j] + Math.max(dp[0][i-1][j], dp[0][i-1][j+1]);
-                        dp[1][i][j] = map[i][j] + Math.min(dp[1][i-1][j], dp[1][i-1][j+1]);
-                        break;
-                    case 1 :
-                        dp[0][i][j] = map[i][j] + Math.max(dp[0][i-1][j-1], Math.max(dp[0][i-1][j], dp[0][i-1][j+1]));
-                        dp[1][i][j] = map[i][j] + Math.min(dp[1][i-1][j-1], Math.min(dp[1][i-1][j], dp[1][i-1][j+1]));
-                        break;
-                    case 2 :
-                        dp[0][i][j] = map[i][j] + Math.max(dp[0][i-1][j-1], dp[0][i-1][j]);
-                        dp[1][i][j] = map[i][j] + Math.min(dp[1][i-1][j-1], dp[1][i-1][j]);
-                        break;
-                }
-            }
-        }
-
-        int max = 0;
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0 ; i < 3 ; i++) {
-            max = Math.max(max, dp[0][N-1][i]);
-            min = Math.min(min, dp[1][N-1][i]);
-        }
-
-        sb.append(max).append(" ").append(min);
+    static void init() {
+        map = new int[N][3];
+        maxDP = new int[N][3];
+        minDP = new int[N][3];
     }
 }
