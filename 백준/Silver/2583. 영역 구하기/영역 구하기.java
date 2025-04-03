e16c787829fc61f8eafb0dacbe06a7d36a61d433
hyeongseok Lee <gudtjr2949@naver.com>
2025-04-03 11:40:30 +0900

    [Silver I] Title: 영역 구하기, Time: 132 ms, Memory: 16432 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,87 +1,87 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M, K;
+    static int N, M, K, size;
+    static int[] dx = {0, 1, 0, -1}, dy = {-1, 0, 1, 0};
+    static List<Integer> list;
     static int[][] map;
     static boolean[][] visited;
-    static int[] nx = {0, 1, 0, -1}, ny = {-1, 0, 1, 0};
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        System.out.println(list.size());
+        for (int size : list) {
+            System.out.print(size + " ");
+        }
+    }
+
+    static void solve() {
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = 0 ; j < M ; j++) {
+                if (map[i][j] == 0 && !visited[i][j]) {
+                    size = 1;
+                    visited[i][j] = true;
+                    dfs(j, i);
+                    list.add(size);
+                }
+            }
+        }
+
+        Collections.sort(list);
+    }
+
+    static void dfs(int x, int y) {
+        for (int i = 0 ; i < 4 ; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (isRange(nx, ny) && map[ny][nx] == 0 && !visited[ny][nx]) {
+                visited[ny][nx] = true;
+                size++;
+                dfs(nx, ny);
+            }
+        }
+    }
+
+    static boolean isRange(int x, int y) {
+        return x >= 0 && x < M && y >= 0 && y < N;
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
-        map = new int[N][M];
-        visited = new boolean[N][M];
-
+        init();
         for (int i = 0 ; i < K ; i++) {
             st = new StringTokenizer(bf.readLine());
-
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-
-            for (int j = y1 ; j < y2 ; j++) {
-                for (int q = x1 ; q < x2 ; q++) {
-                    map[j][q] = -1;
-                    visited[j][q] = true;
-                }
-            }
+            makeMap(x1, y1, x2, y2);
         }
-
-        int cnt = 1;
-
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = 0 ; j < M ; j++) {
-                if (map[i][j] == 0) {
-                    dfs(j, i, cnt);
-                    cnt++;
-                }
-            }
-        }
-
-        int[] size = new int[cnt];
-
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = 0 ; j < M ; j++) {
-                if (map[i][j] > 0) {
-                    size[map[i][j]]++;
-                }
-            }
-        }
-
-        Arrays.sort(size);
-
-        System.out.println(cnt-1);
-
-        for (int i = 1 ; i < cnt ; i++) {
-            System.out.print(size[i] + " ");
-        }
-
     }
 
-    static void dfs(int x, int y, int num) {
-
-        map[y][x] = num;
-        visited[y][x] = true;
-
-        for (int i = 0 ; i < 4 ; i++) {
-            int dx = x + nx[i];
-            int dy = y + ny[i];
-
-            if (dx >= 0 && dx < M && dy >= 0 && dy < N && !visited[dy][dx] && map[dy][dx] == 0) {
-                visited[dy][dx] = true;
-                map[dy][dx] = num;
-                dfs(dx, dy, num);
+    static void makeMap(int x1, int y1, int x2, int y2) {
+        for (int i = y1 ; i < y2 ; i++) {
+            for (int j = x1 ; j < x2 ; j++) {
+                map[i][j] = 1;
             }
         }
+    }
+
+    static void init() {
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        list = new ArrayList<>();
     }
 }
