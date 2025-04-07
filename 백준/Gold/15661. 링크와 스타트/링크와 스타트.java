c7e10bf16b087fad766447bc94d9f04ffaf7aaa5
hyeongseok Lee <gudtjr2949@naver.com>
2025-04-07 12:07:04 +0900

    [Gold V] Title: 링크와 스타트, Time: 916 ms, Memory: 19944 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -1,79 +1,77 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, answer = Integer.MAX_VALUE;
+    static int N, team, answer = Integer.MAX_VALUE;
     static int[][] S;
     static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        System.out.println(answer);
+    }
+
+    static void solve() {
+        for (int i = 1 ; i <= N/2 ; i++) {
+            team = i;
+            dfs(0, 0);
+            if (answer == 0) return;
+        }
+    }
+
+    static void dfs(int idx, int cur) {
+        if (idx == team) {
+            int diff = calOverall();
+            answer = Math.min(answer, diff);
+            return;
+        }
+
+        for (int i = cur ; i < N ; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(idx+1, i+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    static int calOverall() {
+        int A = 0;
+        int B = 0;
+
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = i+1 ; j < N ; j++) {
+                if (i == j) continue;
+
+                if (visited[i] && visited[j]) {
+                    A += S[i][j] + S[j][i];
+                } else if (!visited[i] && !visited[j]) {
+                    B += S[i][j] + S[j][i];
+                }
+            }
+        }
+
+        return Math.abs(A - B);
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(bf.readLine());
-        S = new int[N][N];
-        visited = new boolean[N];
-
+        init();
         for (int i = 0 ; i < N ; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             for (int j = 0 ; j < N ; j++) {
                 S[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        dfs(0, 0);
-
-        System.out.println(answer);
     }
 
-    static void dfs(int idx, int start) {
-        if (idx == N) {
-            findAnswer();
-            return;
-        }
-
-        visited[start] = true;
-        dfs(idx+1, start+1);
-        visited[start] = false;
-        dfs(idx+1, start+1);
-    }
-
-    static void findAnswer() {
-        List<Integer> teamA = new ArrayList<>();
-        List<Integer> teamB = new ArrayList<>();
-
-        for (int i = 0 ; i < N ; i++) {
-            if (visited[i]) {
-                teamA.add(i);
-            } else {
-                teamB.add(i);
-            }
-        }
-
-        int teamASum = 0;
-        int teamBSum = 0;
-
-        for (int i = 0 ; i < teamA.size()-1 ; i++) {
-            int player1 = teamA.get(i);
-
-            for (int j = i+1 ; j < teamA.size() ; j++) {
-                int player2 = teamA.get(j);
-                teamASum += S[player1][player2] + S[player2][player1];
-            }
-        }
-
-        for (int i = 0 ; i < teamB.size()-1 ; i++) {
-            int player1 = teamB.get(i);
-
-            for (int j = i+1 ; j < teamB.size() ; j++) {
-                int player2 = teamB.get(j);
-                teamBSum += S[player1][player2] + S[player2][player1];
-            }
-        }
-
-        answer = Math.min(answer, Math.abs(teamASum - teamBSum));
+    static void init() {
+        S = new int[N][N];
+        visited = new boolean[N];
     }
 }
