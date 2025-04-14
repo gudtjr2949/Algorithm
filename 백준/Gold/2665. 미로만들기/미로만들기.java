00848d59410143c9acc20110c0daa6b536f99974
hyeongseok Lee <gudtjr2949@naver.com>
2025-04-14 11:15:18 +0900

    [Gold IV] Title: 미로만들기, Time: 112 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -1,84 +1,78 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Main {
 
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dy = {-1, 0, 1, 0}, dx = {0, 1, 0, -1};
-    static boolean findAnswer = false;
-    static class Point implements Comparable<Point> {
-        int x, y, destroy;
-
-        public Point(int x, int y, int destroy) {
+    static int N, answer;
+    static int[] dx = {0, 1, 0, -1}, dy = {-1, 0, 1, 0};
+    static int[][] map, dp;
+    static class Node {
+        int x, y, cnt;
+        public Node(int x, int y, int cnt) {
             this.x = x;
             this.y = y;
-            this.destroy = destroy;
-        }
-
-        @Override
-        public int compareTo(Point p) {
-            return p.destroy - this.destroy;
+            this.cnt = cnt;
         }
     }
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        System.out.println(answer);
+    }
+
+    static void solve() {
+        Queue<Node> PQ = new PriorityQueue<>((o1, o2) -> o1.cnt - o2.cnt);
+        PQ.add(new Node(0, 0, 0));
+        dp[0][0] = 0;
+
+        while (!PQ.isEmpty()) {
+            Node now = PQ.poll();
+
+            if (now.x == N-1 && now.y == N-1) {
+                answer = now.cnt;
+                return;
+            }
+
+            for (int i = 0 ; i < 4 ; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+
+                if (!isRange(nx, ny)) continue;
+
+                if (map[ny][nx] == 1 && dp[ny][nx] > now.cnt) {
+                    dp[ny][nx] = now.cnt;
+                    PQ.add(new Node(nx, ny, now.cnt));
+                } else if (map[ny][nx] == 0 && dp[ny][nx] > now.cnt+1) {
+                    dp[ny][nx] = now.cnt+1;
+                    PQ.add(new Node(nx, ny, now.cnt+1));
+                }
+            }
+        }
+    }
+
+    static boolean isRange(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < N;
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
-        map = new int[N][N];
-
+        init();
         for (int i = 0 ; i < N ; i++) {
             String s = bf.readLine();
             for (int j = 0 ; j < N ; j++) {
                 map[i][j] = s.charAt(j) - '0';
             }
         }
-
-        int destroy = 0;
-
-        while (true) {
-            bfs(destroy);
-            if (findAnswer) {
-                System.out.println(destroy);
-                break;
-            }
-            destroy++;
-        }
     }
 
-    static void bfs(int destroy) {
-        PriorityQueue<Point> PQ = new PriorityQueue<>();
-        visited = new boolean[N][N];
-
-        PQ.add(new Point(0, 0, destroy));
-        visited[0][0] = true;
-
-        while (!PQ.isEmpty()) {
-            Point p = PQ.poll();
-
-            if (p.x == N-1 && p.y == N-1) {
-                findAnswer = true;
-                return;
-            }
-
-            for (int i = 0 ; i < 4 ; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-
-                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[ny][nx]) {
-                    if (map[ny][nx] == 0 && p.destroy > 0) { // 흰 방 -> 검은 방
-                        visited[ny][nx] = true;
-                        PQ.add(new Point(nx, ny, p.destroy-1));
-                    } else if (map[ny][nx] == 1) { // 그냥 흰 방으로 이동
-                        visited[ny][nx] = true;
-                        PQ.add(new Point(nx, ny, p.destroy));
-                    }
-                }
-            }
-        }
+    static void init() {
+        map = new int[N][N];
+        dp = new int[N][N];
+        for (int i = 0 ; i < N ; i++) Arrays.fill(dp[i], 2501);
     }
 }
