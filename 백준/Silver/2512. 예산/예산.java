c3ff064b896e482398afe153647e9dbb8b035bec
hyeongseok Lee <gudtjr2949@naver.com>
2025-04-15 11:39:18 +0900

    [Silver II] Title: 예산, Time: 140 ms, Memory: 15128 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -4,65 +4,59 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M, max = 0;
+    static int N, M, MAX = 0, answer;
     static int[] arr;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
-
-        arr = new int[N];
-
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        int sum = 0;
-        max = 0;
-
-        for (int i = 0 ; i < N ; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            sum += arr[i];
-            max = Math.max(max, arr[i]);
-        }
-
-        M = Integer.parseInt(bf.readLine());
-
-        if (sum <= M) {
-            System.out.println(max);
-        } else {
-            binarySearch();
-        }
-    }
-
-    static void binarySearch() {
-        int left = 0;
-        int right = max;
-        int answer = 0;
-
-        while (left <= right) {
-            int mid = (left + right) / 2; // 각 도시에 나눠줄 예산
-
-            if (solve(mid) > M) {
-                right = mid - 1;
-            } else {
-                answer = mid;
-                left = mid + 1;
-            }
-        }
-
+        input();
+        solve();
         System.out.println(answer);
     }
 
-    static int solve(int mid) {
+    static void solve() {
+        int left = 0;
+        int right = MAX;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int sum = calMoney(mid);
+            if (sum <= M) {
+                left = mid+1;
+            } else {
+                right = mid-1;
+            }
+        }
+
+        answer = right;
+    }
+
+    static int calMoney(int mid) {
         int sum = 0;
 
         for (int i = 0 ; i < N ; i++) {
-            if (arr[i] <= mid) {
-                sum += arr[i];
-            } else {
+            if (arr[i] > mid) {
                 sum += mid;
+            } else {
+                sum += arr[i];
             }
         }
 
         return sum;
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        init();
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            MAX = Math.max(MAX, arr[i]);
+        }
+        M = Integer.parseInt(bf.readLine());
+    }
+
+    static void init() {
+        arr = new int[N];
     }
 }
