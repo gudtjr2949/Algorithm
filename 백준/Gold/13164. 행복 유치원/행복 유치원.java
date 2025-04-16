da4da39934e40a97623a77bf460c080380a73c58
hyeongseok Lee <gudtjr2949@naver.com>
2025-04-16 11:58:43 +0900

    [Gold V] Title: 행복 유치원, Time: 732 ms, Memory: 64408 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -1,43 +1,49 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
     static int N, K;
     static long answer;
-    static int[] arr, diffArr;
+    static long[] arr;
+    static List<Long> diff;
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        System.out.println(answer);
+    }
+
+    static void solve() {
+        Arrays.sort(arr);
+
+        for (int i = 0 ; i < N-1 ; i++) {
+            diff.add(arr[i+1] - arr[i]);
+            answer += arr[i+1] - arr[i];
+        }
+
+        Collections.sort(diff, Collections.reverseOrder());
+
+        for (int i = 0 ; i < K-1 ; i++) {
+            answer -= diff.get(i);
+        }
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
-        arr = new int[N];
-        diffArr = new int[N-1];
-
+        init();
         st = new StringTokenizer(bf.readLine());
-
         for (int i = 0 ; i < N ; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Long.parseLong(st.nextToken());
         }
-
-        solve();
-
-        System.out.println(answer);
     }
 
-    private static void solve() {
-        for (int i = 0 ; i < N-1 ; i++) {
-            diffArr[i] = arr[i+1] - arr[i];
-        }
-
-        Arrays.sort(diffArr);
-
-        for (int i = 0 ; i < N-K ; i++) {
-            answer += diffArr[i];
-        }
+    static void init() {
+        arr = new long[N];
+        diff = new ArrayList<>();
     }
 }
