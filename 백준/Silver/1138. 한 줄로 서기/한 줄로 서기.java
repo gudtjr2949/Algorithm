d2a6ba19c450e6e3f71f04cef943c063d0591765
hyeongseok Lee <gudtjr2949@naver.com>
2025-04-16 11:13:58 +0900

    [Silver II] Title: 한 줄로 서기, Time: 120 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,67 +1,46 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
-    static boolean find;
-    static StringBuilder sb;
-    static int[] arr, input;
-    static boolean[] visited;
+    static int[] arr, answer;
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        for (int i = 0 ; i < N ; i++) {
+            System.out.print(answer[i] + " ");
+        }
+    }
+
+    static void solve() {
+        for (int i = 0 ; i < N ; i++) {
+            int cnt = 0;
+            for (int j = 0 ; j < N ; j++) {
+                if (cnt == arr[i] && answer[j] == 0) {
+                    answer[j] = i+1;
+                    break;
+                }
+
+                if (answer[j] == 0) cnt++;
+            }
+        }
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
-
-        arr = new int[N+1];
-        input = new int[N+1];
-        visited = new boolean[N+1];
-        sb = new StringBuilder();
-
+        init();
         StringTokenizer st = new StringTokenizer(bf.readLine());
-
-        for (int i = 1 ; i <= N ; i++) {
+        for (int i = 0 ; i < N ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        dfs(1);
-
-        System.out.println(sb);
     }
 
-    static void dfs(int idx) {
-        if (idx == N+1) {
-            if (check()) {
-                find = true;
-                for (int i = 1 ; i <= N ; i++) sb.append(input[i]).append(" ");
-            }
-            return;
-        }
-
-        if (find) return;
-
-        for (int i = 1 ; i <= N ; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                input[idx] = i;
-                dfs(idx+1);
-                visited[i] = false;
-            }
-        }
-    }
-
-    static boolean check() {
-        for (int i = 1 ; i <= N ; i++) {
-            int cnt = 0;
-            for (int j = 1 ; j < i ; j++) {
-                if (input[i] < input[j]) cnt++;
-            }
-
-            if (arr[input[i]] != cnt) return false;
-        }
-
-        return true;
+    static void init() {
+        arr = new int[N];
+        answer = new int[N];
     }
 }
