086bac1d68e5ab7bc56871c7292184cd2475d9dc
hyeongseok Lee <gudtjr2949@naver.com>
2025-04-18 11:49:21 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 328 ms, Memory: 32892 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,47 +1,54 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, K, answer;
-    static int[] arr, cnt;
+    static int[] cnt, arr;
+
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        System.out.println(answer);
+    }
+
+    static void solve() {
+        int left = 0;
+        int right = 0;
+        cnt[arr[0]]++;
+
+        while (right+1 < N && left+1 < N) {
+            if (cnt[arr[right+1]]+1 <= K) {
+                cnt[arr[right+1]]++;
+                right++;
+            } else {
+                cnt[arr[left]]--;
+                left++;
+            }
+
+            answer = Math.max(answer, right-left+1);
+        }
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
-        arr = new int[N];
+        init();
         st = new StringTokenizer(bf.readLine());
-
         int max = 0;
         for (int i = 0 ; i < N ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             max = Math.max(max, arr[i]);
         }
-
         cnt = new int[max+1];
-
-        solve();
-
-        System.out.println(answer);
     }
 
-    static void solve() {
-        int start = 0;
-        int end = 0;
-
-        while (end < N && start <= end) {
-            if (cnt[arr[end]] >= K) {
-                cnt[arr[start]]--;
-                start++;
-            } else {
-                cnt[arr[end]]++;
-                answer = Math.max(answer, end - start + 1);
-                end++;
-            }
-        }
+    static void init() {
+        arr = new int[N];
     }
 }
