b98c8135be4e09857f4fcb84147c0401ff887724
hyeongseok Lee <gudtjr2949@naver.com>
2025-04-19 16:34:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53608 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,59 +1,54 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, K, answer;
+    static int N, K;
     static int[][] dp;
     static Node[] nodes;
     static class Node {
-        int w, v;
-
-        public Node(int w, int v) {
-            this.w = w;
-            this.v = v;
+        int weight, cost;
+        public Node(int weight, int cost) {
+            this.weight = weight;
+            this.cost = cost;
         }
     }
 
     public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        System.out.println(dp[N][K]);
+    }
+
+    static void solve() {
+        for (int i = 1 ; i <= N ; i++) {
+            for (int j = 0 ; j <= K ; j++) {
+                if (nodes[i].weight > j) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], nodes[i].cost + dp[i-1][j-nodes[i].weight]);
+                }
+            }
+        }
+    }
+
+    static void input() throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
-        dp = new int[K+1][N+1];
-
-        nodes = new Node[N];
-        for (int i = 0 ; i < N ; i++) {
+        init();
+        for (int i = 1 ; i <= N ; i++) {
             st = new StringTokenizer(bf.readLine());
-            nodes[i] = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            int weight = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            nodes[i] = new Node(weight, cost);
         }
-
-        Arrays.sort(nodes, (o1, o2) -> {
-            if (o1.w == o2.w) return o1.v - o2.v;
-            return o1.w - o2.w;
-        });
-
-        solve();
-
-        System.out.println(answer);
     }
 
-    static void solve() {
-       for (int i = 1 ; i <= K ; i++) {
-           for (int j = 1 ; j <= N ; j++) {
-               if (i < nodes[j-1].w) {
-                   dp[i][j] = dp[i][j-1];
-               } else {
-                   dp[i][j] = Math.max(dp[i-nodes[j-1].w][j-1] + nodes[j-1].v, dp[i][j-1]);
-               }
-           }
-       }
-
-       for (int i = 1 ; i <= N ; i++) {
-           answer = Math.max(answer, dp[K][i]);
-       }
+    static void init() {
+        nodes = new Node[N+1];
+        dp = new int[N+1][K+1];
     }
 }
