95a849565feb99bb629db6f5a733d3f186c499a2
hyeongseok Lee <gudtjr2949@naver.com>
2025-04-19 18:04:24 +0900

    [Gold III] Title: 앱, Time: 224 ms, Memory: 55776 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -5,9 +5,9 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M, answer = Integer.MAX_VALUE;
-    static Node[] app;
+    static int N, M, MAX = 100_001, answer;
     static int[][] dp;
+    static Node[] apps;
     static class Node {
         int memory, cost;
         public Node(int memory, int cost) {
@@ -17,45 +17,54 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        app = new Node[N];
-
-        st = new StringTokenizer(bf.readLine());
-        StringTokenizer st2 = new StringTokenizer(bf.readLine());
-
-        for (int i = 0 ; i < N ; i++) {
-            int m = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st2.nextToken());
-            app[i] = new Node(m, c);
-        }
-
-        dp = new int[N][100_001];
-
-        Arrays.sort(app, (o1, o2) -> {
-            if (o1.memory == o2.memory) return o1.cost - o2.cost;
-            return o1.memory - o2.memory;
-        });
-
+        input();
         solve();
-
         System.out.println(answer);
     }
 
     static void solve() {
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = 0 ; j < 100_001 ; j++) {
-                if (i == 0) {
-                    if (j - app[i].cost >= 0) dp[i][j] = app[i].memory;
+        answer = MAX;
+
+        Arrays.sort(apps, (o1, o2) -> {
+            if (o1.memory == o2.memory) return o1.cost - o2.cost;
+            return o1.memory - o2.memory;
+        });
+
+        for (int i = 1 ; i <= N ; i++) {
+            for (int j = 0 ; j < MAX ; j++) {
+                if (apps[i].cost > j) {
+                    dp[i][j] = dp[i-1][j];
                 } else {
-                    if (j - app[i].cost >= 0) dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-app[i].cost] + app[i].memory);
-                    else dp[i][j] = dp[i-1][j];
+                    dp[i][j] = Math.max(dp[i-1][j], apps[i].memory + dp[i-1][j-apps[i].cost]);
                 }
 
-                if (dp[i][j] >= M) answer = Math.min(answer, j);
+                if (M <= dp[i][j]) {
+                    answer = Math.min(answer, j);
+                }
             }
         }
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        init();
+
+        st = new StringTokenizer(bf.readLine());
+        StringTokenizer st2 = new StringTokenizer(bf.readLine());
+
+        apps[0] = new Node(0, 0);
+        for (int i = 1 ; i <= N ; i++) {
+            int memory = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st2.nextToken());
+            apps[i] = new Node(memory, cost);
+        }
+    }
+
+    static void init() {
+        dp = new int[N+1][MAX];
+        apps = new Node[N+1];
     }
 }
