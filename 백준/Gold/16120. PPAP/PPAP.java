2866413197a63db6de3d3fe100b23311197f3b6f
hyeongseok Lee <gudtjr2949@naver.com>
2025-05-01 12:16:17 +0900

    [Gold IV] Title: PPAP, Time: 408 ms, Memory: 25252 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.java
+++ 백준/Gold/16120. PPAP/PPAP.java
@@ -4,36 +4,52 @@ import java.util.Stack;
 
 public class Main {
 
-    static boolean possible;
     static String s;
     static Stack<Character> stack = new Stack<>();
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        s = bf.readLine();
-        possible = true;
-        solve();
-
-        if(stack.size() == 1 && possible) {
+        input();
+        if (solve()) {
             System.out.println("PPAP");
         } else {
             System.out.println("NP");
         }
     }
 
-    static void solve() {
-        for(int i=0; i< s.length(); i++) {
-            if(s.charAt(i) == 'P') {
-                stack.push('P');
-            } else {
-                if(stack.size() >= 2 && i < s.length() -1 && s.charAt(i+1) == 'P') {
-                    stack.pop();
-                    stack.pop();
-                } else {
-                    possible = false;
-                    return;
-                }
+    static boolean solve() {
+        for (int i = 0 ; i < s.length() ; i++) {
+            stack.add(s.charAt(i));
+
+            if (check()) {
+                for (int j = 0 ; j < 4 ; j++) stack.pop();
+                stack.add('P');
             }
         }
+
+        if (stack.size() == 1 && stack.peek() == 'P') {
+            return true;
+        }
+
+        return false;
+    }
+
+    static boolean check() {
+        if (stack.size() < 4) return false;
+
+        int idx = stack.size()-1;
+
+        if (stack.get(idx) == 'P'
+                && stack.get(idx-1) == 'A'
+                && stack.get(idx-2) == 'P'
+                && stack.get(idx-3) == 'P') {
+            return true;
+        }
+
+        return false;
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        s = bf.readLine();
     }
 }
