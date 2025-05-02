4bc0bb938f978780397bcefc478c79726454fe16
hyeongseok Lee <gudtjr2949@naver.com>
2025-05-02 12:32:42 +0900

    [Gold IV] Title: 램프, Time: 112 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.java
+++ 백준/Gold/1034. 램프/램프.java
@@ -5,54 +5,57 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, M, K, answer;
-    static char[][] map;
+    static String[] arr;
+    static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new char[N][M];
-
-        for (int i = 0 ; i < N ; i++) {
-            String s = bf.readLine();
-            for (int j = 0 ; j < M ; j++) {
-                map[i][j] = s.charAt(j);
-            }
-        }
-
-        K = Integer.parseInt(bf.readLine());
-
+        input();
         solve();
-
         System.out.println(answer);
     }
 
     static void solve() {
         for (int i = 0 ; i < N ; i++) {
-            int cnt = 0;
-            StringBuilder standard = new StringBuilder();
+            if (!visited[i] && check(i)) {
+                int cnt = 0;
 
-            for (int j = 0 ; j < M ; j++) {
-                standard.append(map[i][j]);
-                if (map[i][j] == '0') cnt++;
-            }
-
-            int sum = 0;
-
-            if (cnt <= K && (K % 2 != 0 && cnt % 2 != 0 || K % 2 == 0 && cnt % 2 == 0)) {
-                for (int j = 0 ; j < N ; j++) {
-                    StringBuilder tmp = new StringBuilder();
-
-                    for (int k = 0 ; k < M ; k++) {
-                        tmp.append(map[j][k]);
+                for (int j = i ; j < N ; j++) {
+                    if (arr[i].equals(arr[j])) {
+                        visited[j] = true;
+                        cnt++;
                     }
-
-                    if (standard.toString().equals(tmp.toString())) sum++;
                 }
+                answer = Math.max(answer, cnt);
             }
-
-            answer = Math.max(answer, sum);
         }
+    }
+
+    static boolean check(int idx) {
+        int zero = 0;
+        for (int i = 0 ; i < M ; i++) {
+            if (arr[idx].charAt(i) == '0') zero++;
+        }
+
+        if (zero <= K && ((K%2 != 0 && zero%2 != 0) || (K%2 == 0 && zero%2 == 0))) return true;
+
+        return false;
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        init();
+        for (int i = 0 ; i < N ; i++) {
+            String s = bf.readLine();
+            arr[i] = s;
+        }
+        K = Integer.parseInt(bf.readLine());
+    }
+
+    static void init() {
+        arr = new String[N];
+        visited = new boolean[N];
     }
 }
