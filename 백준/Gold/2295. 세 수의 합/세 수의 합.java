3b8f95fbe830b69ff389861989e4646afb89774c
hyeongseok Lee <gudtjr2949@naver.com>
2025-05-09 12:03:17 +0900

    [Gold IV] Title: 세 수의 합, Time: 364 ms, Memory: 62796 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -1,69 +1,52 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
-    static int N;
-    static int[] arr, sumArr;
-    static long answer = 0;
+    static int N, answer;
+    static int[] arr;
+    static Set<Integer> set;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
-
-        arr = new int[N];
-
-        sumArr = new int[N*N];
-
-        for (int i = 0 ; i < N ; i++) {
-            arr[i] = Integer.parseInt(bf.readLine());
-        }
-
-        int idx = 0;
-
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = 0 ; j < N ; j++) {
-                sumArr[idx++] = arr[i] + arr[j];
-            }
-        }
-
-        Arrays.sort(arr);
-        Arrays.sort(sumArr);
-
-        for (int i = N-1 ; i >= 0 ; i--) {
-            for (int j = 0 ; j < i ; j++) {
-                int gap = arr[i] - arr[j];
-
-                // gap 이 sum 배열 안에 있는지 검사
-                if (binarySearch(gap)) {
-                    answer = Math.max(answer, arr[i]);
-                }
-            }
-
-        }
-
-
+        input();
+        solve();
         System.out.println(answer);
     }
 
-    // sum 에 있는 3개의 수를 합해서 num 을 만들수 있는지 판별하는 메서드
-    static boolean binarySearch(int num) {
-        int left = 0;
-        int right = sumArr.length-1;
+    static void solve() {
+        Arrays.sort(arr);
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            if (sumArr[mid] < num) { // sum 이 num 보다 작음 -> sum 이 더 커져야 함 -> left 가 더 커져야 함
-                left = mid + 1;
-            } else if (sumArr[mid] > num){
-                right = mid - 1;
-            } else {
-                return true;
+        for(int i = 0 ; i < N ; i++){
+            for(int j = i ; j < N ; j++){
+                set.add(arr[i] + arr[j]);
             }
         }
 
-        return false;
+        for(int i = 0 ; i < N ; i++){
+            for(int j = 0 ; j < N ; j++){
+                int tmp = arr[i] - arr[j];
+
+                if (set.contains(tmp)) {
+                    answer = Math.max(answer, arr[i]);
+                }
+            }
+        }
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        init();
+        for (int i = 0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
+        }
+    }
+
+    static void init() {
+        arr = new int[N];
+        set = new HashSet<>();
     }
 }
