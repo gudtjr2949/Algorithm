4543aa54e9dd6849bbb475353ce98b779c2ff2d0
hyeongseok Lee <gudtjr2949@naver.com>
2025-05-10 12:13:53 +0900

    [Gold III] Title: 소수의 연속합, Time: 180 ms, Memory: 26992 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,59 +1,64 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     static int N, answer;
+    static List<Integer> list;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
+        input();
+        makePrimeList();
         solve();
         System.out.println(answer);
     }
 
     static void solve() {
+        int left = 0;
+        int right = 0;
+        if (list.size() == 0) return;
+        
+        int sum = list.get(0);
+
+        while (left < list.size() && right < list.size()) {
+            if (sum < N) {
+                if (right+1 == list.size()) break;
+                right++;
+                sum += list.get(right);
+            } else {
+                if (sum == N) answer++;
+
+                sum -= list.get(left);
+                left++;
+            }
+        }
+    }
+
+    static void makePrimeList() {
         boolean[] prime = new boolean[N+1];
 
-        prime[0] = prime[1] = true;
-
         for (int i = 2 ; i <= Math.sqrt(N) ; i++) {
-
             if (prime[i]) continue;
 
-            for (int j = i*i ; j <= N ; j+=i) {
+            for (int j = i*i ; j <= N ; j+=i) { // 소수가 아닌 수를 지움
                 prime[j] = true;
             }
         }
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1 ; i <= N ; i++) {
+        for (int i = 2 ; i <= N ; i++) {
             if (!prime[i]) list.add(i);
         }
-
-        int[] prefix = new int[list.size()+1];
-
-        for (int i = 1 ; i <= list.size() ; i++) {
-            prefix[i] = list.get(i-1) + prefix[i-1];
-        }
-
-        twoPointer(prefix);
     }
 
-    static void twoPointer(int[] prefix) {
-        int left = 0;
-        int right = 1;
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        init();
+    }
 
-        while (left < right && right < prefix.length && left < prefix.length) {
-            int sum = prefix[right] - prefix[left];
-
-            if (sum == N) answer++;
-
-            if (sum < N) right++;
-            else left++;
-        }
+    static void init() {
+        list = new ArrayList<>();
     }
 }
