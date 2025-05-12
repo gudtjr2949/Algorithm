760125b538de0bba6e29918b74c2d93013959a2a
hyeongseok Lee <gudtjr2949@naver.com>
2025-05-12 11:24:46 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 668 ms, Memory: 50316 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -1,55 +1,54 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, answer;
-    static Node[] arr;
-    static class Node {
-        int start, end;
-        public Node(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
+    static int[][] arr;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
-        arr = new Node[N];
-
-        for (int i = 0 ; i < N ; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            arr[i] = new Node(start, end);
-        }
-
-        Arrays.sort(arr, (n1, n2) -> n1.start - n2.start);
-
+        input();
         solve();
-
         System.out.println(answer);
     }
 
     static void solve() {
-        PriorityQueue<Node> PQ = new PriorityQueue<>((n1, n2) -> n1.end - n2.end);
-        PQ.add(arr[0]);
-        answer = 1;
+        Arrays.sort(arr, (o1, o2) -> o1[0] - o2[0]);
 
-        for (int i = 1 ; i < N ; i++) {
-            if (PQ.peek().end <= arr[i].start) {
-                PQ.poll();
-                PQ.add(arr[i]);
+        Queue<Integer> PQ = new PriorityQueue<>();
+
+        for (int i = 0 ; i < N ; i++) {
+            if (PQ.isEmpty() || PQ.peek() > arr[i][0]) {
+                PQ.add(arr[i][1]);
             } else {
-                PQ.add(arr[i]);
+                while (!PQ.isEmpty() && PQ.peek() <= arr[i][0]) {
+                    PQ.poll();
+                }
+                PQ.add(arr[i][1]);
             }
 
             answer = Math.max(answer, PQ.size());
         }
+    }
+
+    static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        init();
+        for (int i = 0 ; i < N ; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            arr[i][0] = start;
+            arr[i][1] = end;
+        }
+    }
+
+    static void init() {
+        arr = new int[N][2];
     }
 }
