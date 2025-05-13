735be051142e873daef23bab2f6e54f2b602689d
hyeongseok Lee <gudtjr2949@naver.com>
2025-05-13 11:33:37 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -3,35 +3,46 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
+
+    static int N;
+    static String s, answer;
+
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(bf.readLine());
+
         while (T-- > 0) {
-            String s = bf.readLine();
-            Stack<Character> stack = new Stack<>();
-
-            String result = "YES";
-
-            for (int i = 0 ; i < s.length() ; i++) {
-                if (s.charAt(i) == ')') {
-                    if (!stack.isEmpty())
-                        stack.pop();
-                    else {
-                        result = "NO";
-                        break;
-                    }
-                } else {
-                    stack.add('(');
-                }
-            }
-
-            if (!stack.isEmpty()) result = "NO";
-            
-            sb.append(result).append("\n");
+            s = bf.readLine();
+            solve();
+            sb.append(answer).append("\n");
         }
 
         System.out.println(sb);
+    }
+
+    static void solve() {
+        Stack<Character> stack = new Stack<>();
+        boolean flag = true;
+
+        for (int i = 0 ; i < s.length() ; i++) {
+            if (s.charAt(i) == '(') {
+                stack.add('(');
+            } else {
+                if (stack.isEmpty()) {
+                    flag = false;
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        if (!stack.isEmpty()) flag = false;
+
+        if (flag) answer = "YES";
+        else answer = "NO";
     }
 }
